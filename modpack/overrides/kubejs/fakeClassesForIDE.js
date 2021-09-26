974c0846e9d03fe48666a3e8af92504b0363e4eb
PandaBoy444 <PandaBoy444@gmail.com>
2021-09-27 01:13:42 +0300

    Added fake classes to help with IDE stuff

---
+++ modpack/overrides/kubejs/fakeClassesForIDE.js
@@ -0,0 +1,13 @@
+//fake classes for vscode help
+
+/**
+ * Is itemStack that can have amount
+ * Example = 'minecraft:stick' or '5x minecraft:stick'
+ */
+class item { }
+/**
+ * Can be itemStack or tag
+ * Example = 'minecraft:stick' or '#forge:stick'
+ */
+class ingredient { }
+class block { }
