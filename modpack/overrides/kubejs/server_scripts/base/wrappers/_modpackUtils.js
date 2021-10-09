0095cc0f26c598950da7359614fe238340744373
PandaBoy444 <PandaBoy444@gmail.com>
2021-10-09 12:57:59 +0300

    Implemented removeItem and removeAndReplace (server side)

--- modpack/overrides/kubejs/server_scripts/base/wrappers/_modpackUtils.js
+++ modpack/overrides/kubejs/server_scripts/base/wrappers/_modpackUtils.js
@@ -1,14 +1,25 @@
 //priority:1
-
+/**
+ * Remove an item from the game by hiding it and removing recipes for it
+ * Recipes that use it as ingredient will stay so that you can take care of those yourself
+ * @param {*} item 
+ */
 function removeItem(item) {
+    //hide
     global.itemsToHide.push(item)
+    //add tooltip
+    //remove recipes
+    removeRecipe({ output: item })
+    //remove recipes that 
 }
-function removeAndReplace(item,withItem) {
+function removeAndReplace(item, withItem) {
     //replace in recipes
+    replaceInputForRecipes(item, withItem)
+    replaceOutputForRecipes(item, withItem)
     removeItem(item)
 }
 function removeItems(items) {
     items.forEach(item => {
-        removeItem(itemF)
+        removeItem(item)
     });
 }
