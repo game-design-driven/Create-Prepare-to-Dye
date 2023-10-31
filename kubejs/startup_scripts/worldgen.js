8d49c36dafdecfcd505760d72df0a7ff1eaaf145
Yarden Zamir <dev@yarden-zamir.com>
2023-10-31 11:31:36 +0200

    Remove mystical flower generation

--- kubejs/startup_scripts/worldgen.js
+++ kubejs/startup_scripts/worldgen.js
@@ -11,7 +11,10 @@ WorldgenEvents.remove(event => {
   event.printFeatures('fluid_springs')
   event.printFeatures('vegetal_decoration')
   event.printFeatures('top_layer_modification')
-  // event.removeFeatureById('botania:mystical_flower')
+  event.removeFeatureById('vegetal_decoration', [
+    'botania:mystical_flowers',
+    'botania:mystical_mushrooms'
+  ])
   event.removeOres(props => {
     props.blocks = ['minecraft:redstone_ore','minecraft:iron_ore','minecraft:gold_ore', 'minecraft:copper_ore', 'minecraft:lapis_ore', 'minecraft:emerald_ore']
   })  
