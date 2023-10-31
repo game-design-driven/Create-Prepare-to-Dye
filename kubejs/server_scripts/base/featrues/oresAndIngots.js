5355aa4746758d8d163f114e8b1e3a5bf03aa3d8
Yarden Zamir <dev@yarden-zamir.com>
2023-11-01 00:05:05 +0200

    Update oresAndIngots.js

--- kubejs/server_scripts/base/featrues/oresAndIngots.js
+++ kubejs/server_scripts/base/featrues/oresAndIngots.js
@@ -5,6 +5,8 @@ if (feature('iron and redstone basic income')) {
     addItemApplication('redstone_ore',['minecraft:stone', 'nether_wart'])    
     addItemApplication('deepslate_iron_ore',['minecraft:deepslate', 'red_dye'])
     addItemApplication('deepslate_redstone_ore',['minecraft:deepslate', 'nether_wart'])
+
+    addShaped('raw_iron',['red_dye', 'red_dye', '#forge:dyes', 'cobblestone', 'cobblestone'])
 }
 
 if (feature('Ore processing')) {
