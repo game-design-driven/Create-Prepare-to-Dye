c53404e0c2d37572f7407d8a3064b6786a759833
Yarden Zamir <dev@yarden-zamir.com>
2023-11-01 10:26:27 +0200

    Update stacksize.js

--- kubejs/startup_scripts/stacksize.js
+++ kubejs/startup_scripts/stacksize.js
@@ -1,9 +1,7 @@
-if (feature('Minimum Stack Size of 32')) {
-    ItemEvents.modification(event => {
-        event.modify('/.*/', item => {
-            if (item.maxStackSize < 32) {
-                item.maxStackSize = 32
-            }
-        })
+ItemEvents.modification(event => {
+    event.modify('/.*/', item => {
+        if (item.maxStackSize < 32) {
+            item.maxStackSize = 32
+        }
     })
-}
+})
