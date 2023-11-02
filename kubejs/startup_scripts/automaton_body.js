133c0fc6d7a16d7ca6945effe139142d9baba393
Yarden Zamir <dev@yarden-zamir.com>
2023-11-02 16:27:34 +0200

    feat: increase mining speed to 10x vanilla
    #157 #158

--- kubejs/startup_scripts/automaton_body.js
+++ kubejs/startup_scripts/automaton_body.js
@@ -1,7 +1,7 @@
 BlockEvents.modification(e => {
     e.modify('/.*/' , block => {
         block.requiresTool = false
-        block.destroySpeed = Block.getBlock(block.id).defaultDestroyTime() / 3
+        block.destroySpeed = Block.getBlock(block.id).defaultDestroyTime() / 10
         // console.info("Modifying " + block.id + " to have a destroy speed of " + Block.getBlock(block.id).defaultDestroyTime() / 2)
     })
 })
