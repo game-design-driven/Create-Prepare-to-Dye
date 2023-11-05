bfc1031f5aaaa1eb49ce8313cd72fc76bed8d282
Yarden Zamir <dev@yarden-zamir.com>
2023-11-05 18:55:01 +0200

    tweak: wrap Remove and add stone cutting for variant recipes in a feature

--- kubejs/server_scripts/base/featrues/variants.js
+++ kubejs/server_scripts/base/featrues/variants.js
@@ -2,7 +2,9 @@ if (feature('Remove recipes for blocks gotten using varients')) {
     removeRecipe({id: 'create:deploying/large_cogwheel'})
 }
 
-global.variants.forEach(variant => {
-    removeRecipe({output: variant[0]})
-    addStonecutting(variant[0],variant[1])
-});
+if (feature('Remove and add stone cutting for variant recipes')) {
+    global.variants.forEach(variant => {
+        removeRecipe({output: variant[0]})
+        addStonecutting(variant[0],variant[1])
+    });
+}
