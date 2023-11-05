2b0cb107ce46e0191f08745004383a119b73dedc
Yarden Zamir <dev@yarden-zamir.com>
2023-11-05 20:16:16 +0200

    feat: hide colored versions of items

--- kubejs/client_scripts/jei.js
+++ kubejs/client_scripts/jei.js
@@ -467,11 +467,26 @@ JEIEvents.hideItems((event) => {
     event.hide(item);
     console.log("hiding " + item);
   });
-  //hide colored create stuff
+  //hide colored stuff
   global.colors.forEach((color) => {
     event.hide(`create:${color}_valve_handle`)
     if (color!='brown') event.hide(`create:${color}_toolbox`)
     if (color!='white') event.hide(`create:${color}_seat`)
+    if (color!='white') event.hide(`minecraft:${color}_wool`)
+    if (color!='white') event.hide(`minecraft:${color}_bed`)
+    if (color!='white') event.hide(`supplementaries:flag_${color}`)
+    if (color!='white') event.hide(`minecraft:${color}_concrete`)
+    if (color!='white') event.hide(`minecraft:${color}_concrete_powder`)
+    if (color!='white') event.hide(`minecraft:${color}_banner`)
+    if (color!='white') event.hide(`quark:${color}_stool`)
+    event.hide(`quark:${color}_shingles`)
+    event.hide(`minecraft:${color}_candle`)
+    event.hide(`minecraft:${color}_shulker_box`)
+    event.hide(`minecraft:${color}_terracotta`)
+    event.hide(`minecraft:${color}_glazed_terracotta`)
+    event.hide(`supplementaries:candle_holder_${color}`)
+    event.hide(`minecraft:${color}_stained_glass`)
+    event.hide(`quark:${color}_framed_glass`)
   })
 });
 
