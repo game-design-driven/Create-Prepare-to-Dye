c8b4761bfd69d916f7387dd83da1aebc57c94d13
Yarden Zamir <dev@yarden-zamir.com>
2023-11-06 16:28:07 +0200

    fix: only set gameRules first time

--- kubejs/server_scripts/base/featrues/gamerules.js
+++ kubejs/server_scripts/base/featrues/gamerules.js
@@ -1,4 +1,7 @@
 ServerEvents.loaded(e => {
+    if (e.server.persistentData.gameRulesInit) return
+    e.server.persistentData.gameRulesInit = true
+
     e.server.gameRules.set("disableElytraMovementCheck", true)
     e.server.gameRules.set("drowningDamage", false)
     e.server.gameRules.set("fallDamage", false)
