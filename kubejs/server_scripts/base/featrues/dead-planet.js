58003196d03435336ed39369292bd5cbec907b4e
Yarden Zamir <dev@yarden-zamir.com>
2023-11-07 21:45:08 +0200

    feat: add dirt from stick sand and clay recipe

--- kubejs/server_scripts/base/featrues/dead-planet.js
+++ kubejs/server_scripts/base/featrues/dead-planet.js
@@ -2,3 +2,4 @@
 addCrushing(['sand %75','red_dye %5', 'dead_bush %4', 'bone %4'], 'red_sand')
 addMilling(['sand %50', 'dead_bush %10'], 'red_sand')
 addPurify('red_sand', 'minecraft:terracotta', 15)
+addCompacting('dirt',['stick','sand','clay'])
