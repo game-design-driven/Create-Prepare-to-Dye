c8f8fcaf0598f11c514bad635d4c2ab37d324c24
Yarden Zamir <dev@yarden-zamir.com>
2023-11-07 21:40:54 +0200

    feat: added fermented_goop

---
+++ kubejs/startup_scripts/blocks.js
@@ -0,0 +1,7 @@
+StartupEvents.registry('fluid', event => {
+    event.create('fermented_goop')
+            .displayName('Fermented Goop')
+            .thickTexture(0xE0C3BC)
+            .bucketColor(0xE0C3BC)
+            .noBlock()
+})
