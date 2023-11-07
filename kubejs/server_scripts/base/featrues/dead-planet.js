3653d197b88467e344f0205d828eb5f71bc20b67
Yarden Zamir <dev@yarden-zamir.com>
2023-11-07 21:44:33 +0200

    feat: add sand, red dye, bone and dead bush from red sand procoessing

--- kubejs/server_scripts/base/featrues/dead-planet.js
+++ kubejs/server_scripts/base/featrues/dead-planet.js
@@ -1,2 +1,3 @@
 
-addGrow('minecraft:dead_bush', 'minecraft:red_sand', 'bone_meal')
+addCrushing(['sand %75','red_dye %5', 'dead_bush %4', 'bone %4'], 'red_sand')
+addMilling(['sand %50', 'dead_bush %10'], 'red_sand')
