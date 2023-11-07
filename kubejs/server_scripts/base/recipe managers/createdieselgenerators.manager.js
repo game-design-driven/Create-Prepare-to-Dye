da49ae6aef8bb6de19b1162bc1fe917ea24bef99
Yarden Zamir <dev@yarden-zamir.com>
2023-11-07 21:41:46 +0200

    feat: add create diesel generators manager including distillation and fermenting

---
+++ kubejs/server_scripts/base/recipe managers/createdieselgenerators.manager.js
@@ -0,0 +1,44 @@
+function addFermenting(results, ingredients, processingTime) {
+    processingTime = processingTime || 400;
+    let recipe = {
+        type: "createdieselgenerators:basin_fermenting",
+        ingredients: solveIngredients(ingredients),
+        processingTime: processingTime,
+        results: solveResults(results)
+    };
+    modpackRecipes.push(recipe)
+    return recipe;
+}
+function addDistillation(results, ingredients, processingTime, heatRequirement) {
+    processingTime = processingTime || 400;
+    let recipe = {
+        type: "createdieselgenerators:distillation",
+        ingredients: solveIngredients(ingredients),
+        processingTime: processingTime,
+        results: solveResults(results)
+    };
+    if (heatRequirement) recipe.heatRequirement = heatRequirement
+    modpackRecipes.push(recipe)
+    return recipe;
+}
+// {
+//     "type": "createdieselgenerators:distillation",
+//     "ingredients": [
+//       {
+//         "fluidTag": "forge:crude_oil",
+//         "amount": 100
+//       }
+//     ],
+//     "heatRequirement": "heated",
+//     "processingTime": 100,
+//     "results": [
+//       {
+//         "fluid": "createdieselgenerators:diesel",
+//         "amount": 50
+//       },
+//       {
+//         "fluid": "createdieselgenerators:gasoline",
+//         "amount": 50
+//       }
+//     ]
+//   }
