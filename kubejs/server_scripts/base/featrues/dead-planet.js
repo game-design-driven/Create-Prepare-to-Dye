3565762af88f5746398f30176b64c25b8cbff766
Yarden Zamir <dev@yarden-zamir.com>
2023-11-07 21:45:26 +0200

    feat: add white dye from snow and fermented goop

--- kubejs/server_scripts/base/featrues/dead-planet.js
+++ kubejs/server_scripts/base/featrues/dead-planet.js
@@ -1,5 +1,7 @@
 
+addGrow('minecraft:dead_bush', 'minecraft:red_sand', 'bone_meal')
 addCrushing(['sand %75','red_dye %5', 'dead_bush %4', 'bone %4'], 'red_sand')
 addMilling(['sand %50', 'dead_bush %10'], 'red_sand')
 addPurify('red_sand', 'minecraft:terracotta', 15)
 addCompacting('dirt',['stick','sand','clay'])
+addMixing('white_dye',['32x snowball', '10mb kubejs:fermented_goop'])
