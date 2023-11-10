65a6859da2a9be1cd31342fa27fa7ca849ac93a6
Yarden Zamir <dev@yarden-zamir.com>
2023-11-11 01:29:12 +0200

    feat: document finite nature of water

---
+++ kubejs/client_scripts/criticalTooltips.js
@@ -0,0 +1,3 @@
+//tooltips that are specific for the pack and are critical to playing it
+
+addTooltip('water_bucket','Water is the essence of most organic life, useful for many thing, and is not infinite')
