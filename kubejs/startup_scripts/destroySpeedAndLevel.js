ac187f76959ac84650ce5cd2e2e0ee8658304ee9
Yarden Zamir <dev@yarden-zamir.com>
2023-11-14 16:58:28 +0200

    tweak: remove redundent comments

--- kubejs/startup_scripts/destroySpeedAndLevel.js
+++ kubejs/startup_scripts/destroySpeedAndLevel.js
@@ -2,6 +2,5 @@ BlockEvents.modification(e => {
     e.modify('/.*/' , block => {
         block.requiresTool = false
         block.destroySpeed = Block.getBlock(block.id).defaultDestroyTime() / 10
-        // console.info("Modifying " + block.id + " to have a destroy speed of " + Block.getBlock(block.id).defaultDestroyTime() / 2)
     })
 })
