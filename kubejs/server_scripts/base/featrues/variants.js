b294ed34336a5f377edf780c756875300fc9a8db
Yarden Zamir <dev@yarden-zamir.com>
2023-11-15 20:52:00 +0200

    fix: remove old varient recipe that is no longer needed

--- kubejs/server_scripts/base/featrues/variants.js
+++ kubejs/server_scripts/base/featrues/variants.js
@@ -1,13 +1,3 @@
-if (feature('Combine create devices using variants')) {
-    //some logic is in quark's config
-    replaceShaped('4x create:smart_chute', [' b ','pip',' I '],{
-        b:'#forge:plates/brass',
-        p:'create:polished_rose_quartz',
-        i:'#forge:ingots/iron',
-        I:'#forge:plates/iron'
-    })
-}
-
 if (feature('Remove and add stone cutting for variant recipes')) {
     global.variants.forEach(variant => {
         removeRecipe({output: variant[0]})
