575dcfe174f2e90905b15ad2dd9d5252fd0ca3ef
Yarden Zamir <dev@yarden-zamir.com>
2023-11-16 21:44:43 +0200

    tweak: disable patrols and traders using gamerules

--- kubejs/server_scripts/base/featrues/gamerules.js
+++ kubejs/server_scripts/base/featrues/gamerules.js
@@ -9,6 +9,8 @@ ServerEvents.loaded(e => {
     e.server.gameRules.set("doInsomnia", false)
     e.server.gameRules.set("reducedDebugInfo", false)
     e.server.gameRules.set("keepInventory", true)
+    e.server.gameRules.set("doPatrolSpawning", false)
 	e.server.gameRules.set("commandBlockOutput", false)
+	e.server.gameRules.set("doTraderSpawning", false)
 	e.server.gameRules.set("waterSourceConversion", false)
 })
