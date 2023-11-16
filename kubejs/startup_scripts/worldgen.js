8b6aef115d51e5ab95902f70486c98a538925c7d
Yarden Zamir <dev@yarden-zamir.com>
2023-11-16 14:13:36 +0200

    fix: remove orgen by id to try and fix issues where ores still spawn
    #247

--- kubejs/startup_scripts/worldgen.js
+++ kubejs/startup_scripts/worldgen.js
@@ -1,67 +1,68 @@
-WorldgenEvents.remove(event => {
-  event.printFeatures()
-  event.printFeatures('', 'minecraft:plains')
-  event.printFeatures('raw_generation')
-  event.printFeatures('local_modifications')
-  event.printFeatures('underground_structures')
-  event.printFeatures('surface_structures', 'minecraft:overworld')
-  event.printFeatures('strongholds')
-  event.printFeatures('underground_ores')
-  event.printFeatures('underground_decoration')
-  event.printFeatures('fluid_springs')
-  event.printFeatures('vegetal_decoration')
-  event.printFeatures('top_layer_modification')
-  event.removeFeatureById('vegetal_decoration', [
-    'botania:mystical_flowers',
-    'botania:mystical_mushrooms'
-  ])
-  event.removeFeatureById('fluid_springs',[
-    'minecraft:spring_water',
-    'quark:deferred_feature_fluid_springs'
-  ])
-  event.removeFeatureById('underground_ores',[
-    'minecraft:ore_dirt',
-    'ore_gravel',
-    'minecraft:disc_gravel',
-    'create:striated_ores_overworld',
-  ])
-  event.removeOres(props => {
-    props.blocks = ['minecraft:nether_quartz_ore','minecraft:diamond_ore','minecraft:redstone_ore','minecraft:iron_ore','minecraft:gold_ore', 'minecraft:copper_ore', 'minecraft:lapis_ore', 'minecraft:emerald_ore']
-  })  
-})
-WorldgenEvents.add(event => {
-  //   event.addOre((ore) => {
-  //     ore.addTarget('#minecraft:stone_ore_replaceables', 'minecraft:iron_ore')
-  //     ore.count([0, 5])
-  //     ore.size = 45
-  //     ore.worldgenLayer = 'underground_ores'
-  // })
-  
-  // event.addOre((ore) => {
-  //   ore.addTarget('#minecraft:stone_ore_replaceables', 'minecraft:redstone_ore')
-  //   ore.count([1, 5])
-  //   ore.size = 35
-  //   ore.worldgenLayer = 'underground_ores'
-  // })
-  event.addOre(ore => {
-    ore.biomes = '#minecraft:is_nether'
-    ore.addTarget('minecraft:netherrack', 'minecraft:nether_gold_ore')
-    ore.count([20, 25])
-    ore.size = 20
-    ore.worldgenLayer = 'underground_ores'
-  })
-  event.addOre(ore => {
-    ore.biomes = '#minecraft:is_nether'
-    ore.addTarget('minecraft:netherrack', 'minecraft:nether_quartz_ore')
-    ore.count([20, 25])
-    ore.size = 20
-    ore.worldgenLayer = 'underground_ores'
-  })
-  event.addOre(ore => {
-    ore.biomes = '#minecraft:is_nether'
-    ore.addTarget('minecraft:netherrack', 'minecraft:ancient_debris')
-    ore.count([1, 2])
-    ore.size = 1
-    ore.worldgenLayer = 'underground_ores'
-  })
-})
+WorldgenEvents.remove((event) => {
+  event.printFeatures();
+  event.printFeatures("", "minecraft:plains");
+  event.printFeatures("raw_generation");
+  event.printFeatures("local_modifications");
+  event.printFeatures("underground_structures");
+  event.printFeatures("surface_structures", "minecraft:overworld");
+  event.printFeatures("strongholds");
+  event.printFeatures("underground_ores");
+  event.printFeatures("underground_decoration");
+  event.printFeatures("fluid_springs");
+  event.printFeatures("vegetal_decoration");
+  event.printFeatures("top_layer_modification");
+  event.removeFeatureById("vegetal_decoration", [
+    "botania:mystical_flowers",
+    "botania:mystical_mushrooms",
+  ]);
+  event.removeFeatureById("fluid_springs", [
+    "minecraft:spring_water",
+    "quark:deferred_feature_fluid_springs",
+  ]);
+  event.removeFeatureById("underground_ores", [
+    "ore_gravel",
+    "minecraft:disc_gravel",
+    "minecraft:ore_dirt",
+    "minecraft:ore_gravel",
+    "minecraft:ore_granite_upper",
+    "minecraft:ore_granite_lower",
+    "minecraft:ore_diorite_upper",
+    "minecraft:ore_diorite_lower",
+    "minecraft:ore_andesite_upper",
+    "minecraft:ore_andesite_lower",
+    "minecraft:ore_tuff",
+    "minecraft:ore_coal_upper",
+    "minecraft:ore_coal_lower",
+    "minecraft:ore_iron_upper",
+    "minecraft:ore_iron_middle",
+    "minecraft:ore_iron_small",
+    "minecraft:ore_gold",
+    "minecraft:ore_gold_lower",
+    "minecraft:ore_redstone",
+    "minecraft:ore_redstone_lower",
+    "minecraft:ore_diamond",
+    "minecraft:ore_diamond_large",
+    "minecraft:ore_diamond_buried",
+    "minecraft:ore_lapis",
+    "minecraft:ore_lapis_buried",
+    "minecraft:ore_copper",
+    "minecraft:disk_sand",
+    "minecraft:disk_clay",
+    "minecraft:disk_gravel",
+    "create:striated_ores_overworld",
+    "create:zinc_ore",
+    "quark:deferred_feature_underground_ores",
+  ]);
+  event.removeOres((props) => {
+    props.blocks = [
+      "minecraft:nether_quartz_ore",
+      "minecraft:diamond_ore",
+      "minecraft:redstone_ore",
+      "minecraft:iron_ore",
+      "minecraft:gold_ore",
+      "minecraft:copper_ore",
+      "minecraft:lapis_ore",
+      "minecraft:emerald_ore",
+    ];
+  });
+});
