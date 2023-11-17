ce8f412fa81cb345acea4d95c078b37b7180abe8
Yarden Zamir <dev@yarden-zamir.com>
2023-11-17 15:27:54 +0200

    fix: spaceship crashing every time you join your world instead of only first time

--- kubejs/server_scripts/base/featrues/spaceship.js
+++ kubejs/server_scripts/base/featrues/spaceship.js
@@ -1,7 +1,8 @@
 if (feature('Place spaceship structure')) {
 	ServerEvents.loaded((event) => {
-		if(!event.server.persistentData.crashLanded)
+		if(!event.server.persistentData.crashLanded){
 			event.server.persistentData.crashLanded = true;
 			event.server.runCommandSilent("place structure minecraft:spaceship_wreck_main");
+		}
 	})
 }
