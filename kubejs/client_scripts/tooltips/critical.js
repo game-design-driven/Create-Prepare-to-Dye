cbd7049ffccc851e7446091f23100607829e49c3
Yarden Zamir <dev@yarden-zamir.com>
2023-11-19 23:16:34 +0200

    docs: add tooltips for scoria and limestone

--- kubejs/client_scripts/tooltips/critical.js
+++ kubejs/client_scripts/tooltips/critical.js
@@ -1,4 +1,6 @@
 //tooltips that are specific for the pack and are critical to playing it
 
-addTooltip('water_bucket','Water is the essence of most organic life, useful for many things, and is not infinite')addTooltip('red_sand', 'Found abundantly on the planets surface, also seems that the _lava_ here, flowing or source, can be purified with _pure daisy_ to make _red sand_')
+addTooltip('water_bucket','Water is the essence of most organic life, useful for many things, and is not infinite')
+addTooltip('red_sand', 'Found abundantly on the planets surface, also seems that the _lava_ here, flowing or source, can be purified with _pure daisy_ to make _red sand_')
 addTooltip('create:scoria', 'Can be made by searing flowing _chocolate_ with _lava_')
+addTooltip('create:limestone', 'Can be made by searing flowing _honey_ with _lava_')
