aaf740e37d395073bbcc61baa1bcc5d80d300d20
Yarden Zamir <dev@yarden-zamir.com>
2023-11-19 23:16:03 +0200

    docs: add tooltip to red sand

--- kubejs/client_scripts/tooltips/critical.js
+++ kubejs/client_scripts/tooltips/critical.js
@@ -1,3 +1,4 @@
 //tooltips that are specific for the pack and are critical to playing it
 
-addTooltip('water_bucket','Water is the essence of most organic life, useful for many things, and is not infinite')
+addTooltip('water_bucket','Water is the essence of most organic life, useful for many things, and is not infinite')addTooltip('red_sand', 'Found abundantly on the planets surface, also seems that the _lava_ here, flowing or source, can be purified with _pure daisy_ to make _red sand_')
+addTooltip('create:scoria', 'Can be made by searing flowing _chocolate_ with _lava_')
