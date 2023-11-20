8a1b3d08591aa640a55a8c16448aae6b45c08658
Yarden Zamir <dev@yarden-zamir.com>
2023-11-20 11:14:41 +0200

    tweak: reenable tree fertilizer

--- kubejs/server_scripts/base/mods/create.js
+++ kubejs/server_scripts/base/mods/create.js
@@ -11,9 +11,9 @@ if (feature("Fix create copper scaffolding recipe")) {
 //     addMixing('create:chromatic_compound', ['3x obsidian', '3x glowstone_dust', 'create:polished_rose_quartz'], temperature.superHeated)
 //     addAltar('create:chromatic_compound', ['2x create:polished_rose_quartz', 'obsidian'], 7500)
 // }
-if (feature("Remove tree fertilizer")) {
-  removeItem("create:tree_fertilizer");
-}
+// if (feature("Remove tree fertilizer")) {
+//   removeItem("create:tree_fertilizer");
+// }
 if (feature("Remove vanilla honey bottle recipe")) {
   removeRecipe({ id: "minecraft:honey_bottle" });
 }
