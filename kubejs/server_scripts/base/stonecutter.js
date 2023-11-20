4f12196f99b0f8b29b41d513d9cff375adcf396e
Yarden Zamir <dev@yarden-zamir.com>
2023-11-20 20:02:33 +0200

    dev: commit experiments with stonecutter inv

---
+++ kubejs/server_scripts/base/stonecutter.js
@@ -0,0 +1,21 @@
+// const $Util = Java.loadClass("net.minecraft.Util");
+// const $StonecutterScreen = Java.loadClass(
+//   "net.minecraft.client.gui.screens.inventory.StonecutterScreen"
+// );
+// const $stoneCutterMenu = Java.loadClass(
+//     "net.minecraft.world.inventory.StonecutterMenu"
+// )
+// ItemEvents.rightClicked((event) => {
+//   let screen = Client.currentScreen;
+// Client.
+//   Client.setScreen(
+//     new $StonecutterScreen(
+//         new $stoneCutterMenu(
+//             1, 
+//             Client.player.getInventory()
+//         ),
+//         Client.player.inventory,
+//         Component.black('test')
+//     )
+//   );
+// });
