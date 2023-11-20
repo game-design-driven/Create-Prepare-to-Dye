f6b6cb5824550ce7013ebb50d23353ec174ecfdd
Yarden Zamir <dev@yarden-zamir.com>
2023-11-20 18:31:16 +0200

    fix: items not hidden properly

--- kubejs/server_scripts/base/wrappers/_modpackUtils.js
+++ kubejs/server_scripts/base/wrappers/_modpackUtils.js
@@ -5,15 +5,17 @@
  * @param {*} item 
  */
 function removeItem(item) {
-    global.itemsToRemove.push(item) //why was that commented out?
+    global.itemsToRemove.push(item)
     // //add tooltip
     // //remove recipes
     removeRecipe({ output: item })
     removeRecipe({ input: item })
     //remove tags from item
-    removeAllTagsFrom(item)
     //hide
-    hideItem(item)
+    ServerEvents.tags('item', event => {
+        event.removeAllTagsFrom(item)
+        event.add('c:hidden_from_recipe_viewers', item)
+	})
 }
 
 function removeAllRecipesForItem(item) {
