ab4575ad6428676bf0e24df325ef5b3e44618d57
Yarden Zamir <dev@yarden-zamir.com>
2023-11-22 02:14:32 +0200

    fix: starter items every time

--- kubejs/server_scripts/base/featrues/startingItems.js
+++ kubejs/server_scripts/base/featrues/startingItems.js
@@ -4,7 +4,7 @@ if (feature('starter items')) {
         {
             event.player.give(Item.of('create:wrench', 1))
             event.player.give(Item.of(Item.of('create:clipboard', "{Pages:[{Entries:[{Checked:1b,Text:'{\"text\":\"Feed Betsy\"}'},{Checked:1b,Text:'{\"text\":\"Engine safty checks\"}'},{Checked:0b,Text:'{\"text\":\"Landing systems saftey checks\"}'},{Checked:1b,Text:'{\"text\":\"Water Pure Daisy\"}'},{Checked:0b,Text:'{\"text\":\"Think of cool name for company\"}'},{Checked:0b,Text:'{\"text\":\"Don\\'t crash\"}'},{Checked:0b,Text:'{\"text\":\"Ponder life\"}'},{Checked:1b,Text:'{\"text\":\"Fix wrench\"}'},{Checked:1b,Text:'{\"text\":\"Check automated milking system\"}'},{Checked:0b,Text:'{\"text\":\"Decide what to automate first\"}'}]}],PreviouslyOpenedPage:0,RepairCost:0,Type:1,display:{Name:'{\"text\":\"Day 931 - Landing!\",\"italic\":\"false\"}'}}"), 1))
-            event.player.getPersistentData().putBoolean('starter', false)
+            event.player.getPersistentData().putBoolean('starter', true)
         }
     })
 }
