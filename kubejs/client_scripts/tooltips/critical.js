6516be0cc8b8de95109fa7cf1c8b14d4d8a5be03
Yarden Zamir <dev@yarden-zamir.com>
2023-11-25 19:29:09 +0200

    docs: add tooltips to agreements

--- kubejs/client_scripts/tooltips/critical.js
+++ kubejs/client_scripts/tooltips/critical.js
@@ -6,5 +6,7 @@ addTooltip('lava_bucket', 'Found abundantly on the planets surface and undergrou
 addTooltip('create:scoria', 'Can be made by searing flowing _chocolate_ with _lava_')
 addTooltip('create:limestone', 'Can be made by searing flowing _honey_ with _lava_')
 addTooltip('milk_bucket', 'Ever since the invention of modern _engines_, _milk_ has been the building block of civilizations, _rocket fuel for the masses._ Easy to produce, easy to consume, and tasty too!')
-addTooltip("createdieselgenerators:diesel_engine", "The _Engine_, a modern invention, can run on various fuels, from _milk_, a widely used fuel for space travel, to _bio-diesel_, known for its greater efficiency.")
+addTooltip("createdieselgenerators:diesel_engine", "The _Engine_, a modern invention, can run on various fuels, from _milk_, a widely used fuel for space travel, to _bio-diesel_, known for its greater efficiency")
 addTooltip('cauldron', "Can be filled with _water_ or _lava_ by hand or using a _spout_, used in the process of making _salt_ among other things")
+addTooltip('wares:delivery_agreement', "An intergalactic agreement, bound by the United SPACE consumer union plus plus treaty. When completed can (usually) be mixed to acquire more contracts")
+addTooltip('wares:completed_delivery_agreement', "A completed intergalactic agreement, bound by the United SPACE consumer union plus plus treaty. Can (usually) be mixed to acquire more contracts. Or hanged on the wall, pretty piece of paper")
