ee4e966e8a333b572adc2f960d8ed4bf2c9a9519
Yarden Zamir <dev@yarden-zamir.com>
2023-11-25 20:32:31 +0200

    fix: add backwards compatibility for old bfc plates trade deal progression

--- kubejs/server_scripts/base/trading/landingTradeIntroLine.js
+++ kubejs/server_scripts/base/trading/landingTradeIntroLine.js
@@ -1,5 +1,18 @@
 // Black Hole Bagels LLC
 // 64x fermented blob -> Gourmaryllis 16x sugar canes, water bucket, 4x hoppers
+const bcfPlatesOld = getAgreement({
+  paymentItems: [
+    "32x minecraft:andesite",
+    "8x create:belt_connector",
+    "8x create:cogwheel",
+  ], // This is the result of the trade
+  requestedItems: ["64x create:iron_sheet"], // This is the cost of the trade
+  title: "We want plates", // This is the title of the trade
+  orderedAmount: 2, // This is how many times the trade is done, until the deal is considered complete
+  company: "bobs_construction_fleet", // This is the name of the company
+  message:
+    "Hello, I see you are new here in the trading mesh, doesnt matter much, Im from BCF and we want to contract some white plates, willing to pay too", // This is the description of the trade
+});
 const bcfPlates = getAgreement({
   paymentItems: [
     "32x minecraft:andesite",
@@ -277,6 +290,7 @@ const sssHelmetsFixedRates = getAgreement({
 });
 const starterDeals = [bcfPlates, mlcSand, bhbCheese];
 tradeBranch([bcfPlates2, bnwRedstone], [bcfPlates]);
+tradeBranch([bcfPlates2, bnwRedstone], [bcfPlatesOld]);
 tradeBranch(bnwManasteel, bnwRedstone);
 tradeBranch(bnwQuartz, bnwManasteel);
 tradeBranch([bnwCogs, bnwQuartz], [bnwManasteel, bfcPickaxes]);
