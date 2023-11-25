dcbc16abf685b948c71637b112946a14db6ce1b7
Yarden Zamir <dev@yarden-zamir.com>
2023-11-25 20:18:24 +0200

    dev: add support for list based item tooltips

--- kubejs/client_scripts/lang.helpers.js
+++ kubejs/client_scripts/lang.helpers.js
@@ -1,6 +1,13 @@
 // priority: 100
 
 function addTooltip(stack, tooltip) {
-    if (stack === 'minecraft:air') return
-    global.itemTooltips.push([stack, tooltip])
+  if (stack === "minecraft:air") return;
+  //if list
+  if (Array.isArray(stack)) {
+    stack.forEach((element) => {
+      addTooltip(element, tooltip);
+    });
+  } else {
+    global.itemTooltips.push([stack, tooltip]);
+  }
 }
