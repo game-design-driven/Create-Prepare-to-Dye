5827eea2d758f7170aa8b666ed7c432896d25762
Yarden Zamir <dev@yarden-zamir.com>
2023-11-26 15:01:11 +0200

    tweak: remove energy cell and smart cable recipes

--- kubejs/server_scripts/base/mods/appliedEnergistics.js
+++ kubejs/server_scripts/base/mods/appliedEnergistics.js
@@ -54,23 +54,6 @@ if (feature('Quartz recipes')) {
 }
 
 if (feature('ae2 recipes')) {
-    addShapeless(Item.of('ae2:energy_cell', '{internalCurrentPower:200000.0d,internalMaxPower:200000.0d}'), ['4x create:polished_rose_quartz'])
-    addShaped('4x ae2:fluix_smart_cable', [
-        'PPP',
-        'RRR',
-        'PPP'
-    ], {P: '#forge:silicon',R: 'create:polished_rose_quartz'})
-    addShaped('2x ae2:fluix_smart_cable', [
-        'PPP',
-        'RDR',
-        'PPP'
-    ], {P: '#forge:glass',R: 'create:polished_rose_quartz',D: 'diamond'})
-    addAssembly(['4x ae2:fluix_smart_cable %8', '2x ae2:silicon %1','create:polished_rose_quartz %1'], 'create:polished_rose_quartz',[
-        addDeploying('ae2:fluix_smart_cable', 'create:polished_rose_quartz', 'ae2:silicon'),
-        addPressing('ae2:fluix_smart_cable', 'create:polished_rose_quartz'),
-        addCutting('ae2:fluix_smart_cable', 'create:polished_rose_quartz')
-    ], 3)
-
     addShaped('ae2:storage_bus', ['cps'],{
         c: 'ae2:fluix_smart_cable',
         p: 'minecraft:piston',
