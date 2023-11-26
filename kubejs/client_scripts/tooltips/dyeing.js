0af8545180adf97829b6282c4d2b563b17cf2dec
Yarden Zamir <dev@yarden-zamir.com>
2023-11-26 09:04:18 +0200

    docs: add tooltips for dying stone and red sand

--- kubejs/client_scripts/tooltips/dyeing.js
+++ kubejs/client_scripts/tooltips/dyeing.js
@@ -1,1 +1,14 @@
-addTooltip(['redstone','redstone_ore'], "Can be made by dyeing stone with _red dye_")
+addTooltip(
+  ["redstone", "redstone_ore"],
+  "Can be made by dyeing _stone_ with _red dye_"
+);
+addTooltip(["iron_ore"], "Can be made by dyeing _stone_ with _white dye_");
+addTooltip(
+  ["iron_ingot", "raw_iron"],
+  "Can be made by dyeing _red sand_ or _stone_ with _white dye_"
+);
+addTooltip(["copper_ore"], "Can be made by dyeing _stone_ with _orange dye_");
+addTooltip(
+  ["copper_ingot", "raw_copper"],
+  "Can be made by dyeing _red sand_ or _stone_ with _orange dye_"
+);
