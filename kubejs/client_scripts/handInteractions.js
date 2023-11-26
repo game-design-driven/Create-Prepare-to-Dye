50cf12773786eb042adeb39bc1344c0e39739aac
Yarden Zamir <dev@yarden-zamir.com>
2023-11-26 17:55:18 +0200

    feat: add manual refuling support for regular engine

---
+++ kubejs/client_scripts/handInteractions.js
@@ -0,0 +1,32 @@
+BlockEvents.rightClicked("createdieselgenerators:diesel_engine", (event) => {
+  if (
+    event.player.handSlots[0].id == "minecraft:milk_bucket" ||
+    (event.player.handSlots[0].mod == "createdieselgenerators" &&
+      event.player.handSlots[0].id.endsWith("_bucket") &&
+      event.block.getEntityData().Tanks[0].Level.Value == 0)
+  ) {
+    let fluid;
+    if (event.player.handSlots[0].id == "minecraft:milk_bucket") {
+      fluid = "minecraft:milk";
+    } else {
+      fluid = event.player.handSlots[0].id.replace("_bucket", "");
+      event.player.tell(`debug: ${fluid}`);
+    }
+    event.player.setMainHandItem("minecraft:bucket");
+    event.level.runCommandSilent(
+      `playsound minecraft:item.bucket.empty block @a ${event.block.x} ${event.block.y} ${event.block.z} 0.4 1.7`
+    );
+
+    event.block.mergeEntityData({
+      Tanks: [
+        {
+          TankContent: {
+            Amount: 1000,
+            FluidName: fluid,
+          },
+        },
+      ],
+    });
+    event.cancel();
+  }
+});
