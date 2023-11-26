82e17a7e2e0977f93d1c7c00b60125e71864ce93
Yarden Zamir <dev@yarden-zamir.com>
2023-11-26 14:45:42 +0200

    fix: tooltips to reflect energy cell to milk cell change

--- kubejs/client_scripts/tooltips/ae2.js
+++ kubejs/client_scripts/tooltips/ae2.js
@@ -1,14 +1,28 @@
-addTooltip('ae2:toggle_bus', 'A _network cable_ that allows its connection state to be _toggled_ via _redstone._ This allows you to cut off a section of a ME Network. When _redstone signal_ supplied the part enables the connection, _ME Inverted Toggle Bus_ provides the reverse behavior by disabling the connection instead.')
-addTooltip('ae2:inverted_toggle_bus', 'A _network cable_ that allows its connection state to be _toggled_ via _redstone._ This allows you to cut off a section of a ME Network. When _redstone signal_ supplied the part enables the connection, _ME Inverted Toggle Bus_ provides the reverse behavior by disabling the connection instead.')
-addTooltip('ae2:storage_bus', 'The _ME Storage Bus_, when attached to another inventory block in the world lets you _access_ that inventory _via networked functions_. This allows you to use chests, barrels, or other types of item storage in your networks. The storage via the _ME Storage Bus_ is bi-directional, it can both insert, or extract items from the inventory block it is attached to as long as the _ME Storage Bus_ has its _required channel._ The UI allows you to control which items are selected as storable items, this selection has no effect on what items can be extracted once they are in the storage. The Storage Bus will function with nearly any inventory block, including')
-addTooltip('ae2:terminal', "The _ME Terminal_ is a HID which gives you _access_ to items stored in your _ME Network_. This will also include items accessible through _ME Storage Bus_. It has the ability to _sort_ and _search_, as well as _filter_ by using _View Cell_. It requires a _channel_ to function. Can be upgraded into a _ME Crafting Terminal_.")
-addTooltip('ae2:crafting_terminal', "The _ME Terminal_ is a HID which gives you _access_ to items stored in your _ME Network_. This will also include items accessible through _ME Storage Bus_. It has the ability to _sort_ and _search_, as well as _filter_ by using _View Cell_. It requires a _channel_ to function. Can be upgraded into a _ME Crafting Terminal_.")
-addTooltip('ae2:fluix_smart_cable', 'A Network _requires power_, provided by single-use, disposable _energy cells_. The maximum amount of devices in one network is _8_. A cable will show the amount of _channels_ used on connected devices')
-addTooltip('ae2:energy_cell', 'A Network _requires power_, provided by single-use, disposable _energy cells_. The maximum amount of devices in one network is _8_. A cable will show the amount of _channels_ used on connected devices')
-addTooltip('ae2:flawed_budding_quartz', 'The primary way of obtaining _quartz_ is from breaking _quartz crystal clusters_. These grow on _budding quartz_ in a way that is _similar to amethyst_. _Budding quartz decays when growing buds_, but can be recharged using _Polished Rose Quartz_ in a puddle of water. New _budding quartz_ can be created the same way by using a _Block of Quartz_.')
-addTooltip('ae2:chipped_budding_quartz', 'The primary way of obtaining _quartz_ is from breaking _quartz crystal clusters_. These grow on _budding quartz_ in a way that is _similar to amethyst_. _Budding quartz decays when growing buds_, but can be recharged using _Polished Rose Quartz_ in a puddle of water. New _budding quartz_ can be created the same way by using a _Block of Quartz_.')
-addTooltip('ae2:damaged_budding_quartz', 'The primary way of obtaining _quartz_ is from breaking _quartz crystal clusters_. These grow on _budding quartz_ in a way that is _similar to amethyst_. _Budding quartz decays when growing buds_, but can be recharged using _Polished Rose Quartz_ in a puddle of water. New _budding quartz_ can be created the same way by using a _Block of Quartz_.')
-addTooltip('ae2:small_quartz_bud', 'The primary way of obtaining _quartz_ is from breaking _quartz crystal clusters_. These grow on _budding quartz_ in a way that is _similar to amethyst_. _Budding quartz decays when growing buds_, but can be recharged using _Polished Rose Quartz_ in a puddle of water. New _budding quartz_ can be created the same way by using a _Block of Quartz_.')
-addTooltip('ae2:medium_quartz_bud', 'The primary way of obtaining _quartz_ is from breaking _quartz crystal clusters_. These grow on _budding quartz_ in a way that is _similar to amethyst_. _Budding quartz decays when growing buds_, but can be recharged using _Polished Rose Quartz_ in a puddle of water. New _budding quartz_ can be created the same way by using a _Block of Quartz_.')
-addTooltip('ae2:large_quartz_bud', 'The primary way of obtaining _quartz_ is from breaking _quartz crystal clusters_. These grow on _budding quartz_ in a way that is _similar to amethyst_. _Budding quartz decays when growing buds_, but can be recharged using _Polished Rose Quartz_ in a puddle of water. New _budding quartz_ can be created the same way by using a _Block of Quartz_.')
-addTooltip('quartz', 'The primary way of obtaining _quartz_ is from breaking _quartz crystal clusters_. These grow on _budding quartz_ in a way that is _similar to amethyst_. _Budding quartz decays when growing buds_, but can be recharged using _Polished Rose Quartz_ in a puddle of water. New _budding quartz_ can be created the same way by using a _Block of Quartz_.')
+addTooltip(
+  ["ae2:toggle_bus", "ae2:inverted_toggle_bus"],
+  "A _network cable_ that allows its connection state to be _toggled_ via _redstone._ This allows you to cut off a section of a ME Network. When _redstone signal_ supplied the part enables the connection, _ME Inverted Toggle Bus_ provides the reverse behavior by disabling the connection instead."
+);
+addTooltip(
+  "ae2:storage_bus",
+  "The _ME Storage Bus_, when attached to another inventory block in the world lets you _access_ that inventory _via networked functions_. This allows you to use chests, barrels, or other types of item storage in your networks. The storage via the _ME Storage Bus_ is bi-directional, it can both insert, or extract items from the inventory block it is attached to as long as the _ME Storage Bus_ has its _required channel._ The UI allows you to control which items are selected as storable items, this selection has no effect on what items can be extracted once they are in the storage. The Storage Bus will function with nearly any inventory block, including"
+);
+addTooltip(
+  "ae2:terminal",
+  "The _ME Terminal_ is a HID which gives you _access_ to items stored in your _ME Network_. This will also include items accessible through _ME Storage Bus_. It has the ability to _sort_ and _search_, as well as _filter_ by using _View Cell_. It requires a _channel_ to function. Can be upgraded into a _ME Crafting Terminal_."
+);
+addTooltip(
+  ["ae2:fluix_smart_cable", "ae2:energy_cell"],
+  "A Network _requires power_, provided by _milk cells_. The maximum amount of devices in one network is _8_. A cable will show the amount of _channels_ used on connected devices"
+);
+addTooltip(
+  [
+    "ae2:flawed_budding_quartz",
+    "ae2:chipped_budding_quartz",
+    "ae2:damaged_budding_quartz",
+    "ae2:small_quartz_bud",
+    "ae2:medium_quartz_bud",
+    "ae2:large_quartz_bud",
+    "quartz",
+  ],
+  "The primary way of obtaining _quartz_ is from breaking _quartz crystal clusters_. These grow on _budding quartz_ Which, after it decays can be recharged using _Polished Rose Quartz_ in a puddle of water. New _budding quartz_ can be created the same way by using a _Block of Quartz_."
+);
