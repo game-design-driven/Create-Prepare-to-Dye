f0e15a0f6165d974d4fa509179e799ff3cb3b455
Yarden Zamir <dev@yarden-zamir.com>
2023-11-28 21:03:04 +0200

    docs: fix scorchia tooltip

--- kubejs/client_scripts/tooltips/critical.js
+++ kubejs/client_scripts/tooltips/critical.js
@@ -3,7 +3,7 @@
 addTooltip('water_bucket','Water is the essence of most organic life, useful for many things, and is not infinite')
 addTooltip('red_sand', 'Found abundantly on the planets surface, also seems that the _lava_ here, flowing or source, can be purified with _pure daisy_ to make _red sand_')
 addTooltip('lava_bucket', 'Found abundantly on the planets surface and underground, can be purified into _red sand_')
-addTooltip('create:scoria', 'Can be made by searing flowing _chocolate_ with _lava_')
+addTooltip('create:scorchia', 'Can be made by searing flowing _chocolate_ with _lava_')
 addTooltip('cobblestone', 'Can be made by searing flowing _water_ with _lava_')
 addTooltip('create:limestone', 'Can be made by searing flowing _honey_ with _lava_')
 addTooltip('milk_bucket', 'Ever since the invention of modern _engines_, _milk_ has been the building block of civilizations, _rocket fuel for the masses._ Easy to produce, easy to consume, and tasty too! To acquire _milk_ you will need to find a _cow_ and interact with it using a _bucket_, either by hand or using a deployer')
