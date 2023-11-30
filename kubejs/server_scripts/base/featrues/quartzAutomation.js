893e92b1769bba924c5636843747b96a9da1ba90
Yarden Zamir <dev@yarden-zamir.com>
2023-11-30 20:14:12 +0200

    feat: add filling recipe to grow quartz

--- kubejs/server_scripts/base/featrues/quartzAutomation.js
+++ kubejs/server_scripts/base/featrues/quartzAutomation.js
@@ -1,5 +1,8 @@
 if (feature('Make quartz grow with dripping')) {
     addDripping('ae2:medium_quartz_bud', 'ae2:small_quartz_bud', 'water')
+    addFilling('ae2:medium_quartz_bud', 'ae2:small_quartz_bud', 'water')
     addDripping('ae2:large_quartz_bud', 'ae2:medium_quartz_bud', 'water')
+    addFilling('ae2:large_quartz_bud', 'ae2:medium_quartz_bud', 'water')
     addDripping('ae2:quartz_cluster', 'ae2:large_quartz_bud', 'water')
+    addFilling('ae2:quartz_cluster', 'ae2:large_quartz_bud', 'water')
 }
