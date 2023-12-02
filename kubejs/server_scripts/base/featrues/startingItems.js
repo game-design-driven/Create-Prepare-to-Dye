eb7ea018533d73c8d3bd64734edb846605c9f165
Yarden Zamir <dev@yarden-zamir.com>
2023-12-02 16:58:45 +0200

    feat: automaton move fast

--- kubejs/server_scripts/base/featrues/startingItems.js
+++ kubejs/server_scripts/base/featrues/startingItems.js
@@ -1,6 +1,9 @@
 //priority: 0
 if (feature('starter items')) {
     PlayerEvents.loggedIn((event) => {
+        event.player.setAttributeBaseValue('forge:swim_speed', 3)
+
+
         if (!event.player.getPersistentData().getBoolean('starter'))
         {
             event.player.give(Item.of('create:wrench', 1))
