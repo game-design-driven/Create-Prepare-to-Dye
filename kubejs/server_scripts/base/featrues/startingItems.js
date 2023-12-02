b454dd54a213865a31c0badd10bbb9aeb31a15b4
Yarden Zamir <dev@yarden-zamir.com>
2023-12-02 13:37:50 +0200

    feat: give exsisting players starter trades

--- kubejs/server_scripts/base/featrues/startingItems.js
+++ kubejs/server_scripts/base/featrues/startingItems.js
@@ -1,3 +1,4 @@
+//priority: 0
 if (feature('starter items')) {
     PlayerEvents.loggedIn((event) => {
         if (!event.player.getPersistentData().getBoolean('starter'))
@@ -5,13 +6,14 @@ if (feature('starter items')) {
             event.player.give(Item.of('create:wrench', 1))
             event.player.give(Item.of('create:clipboard', "{Pages:[{Entries:[{Checked:1b,Text:'{\"text\":\"Feed Betsy\"}'},{Checked:1b,Text:'{\"text\":\"Engine safty checks\"}'},{Checked:0b,Text:'{\"text\":\"Landing systems saftey checks\"}'},{Checked:1b,Text:'{\"text\":\"Make sure Pure Daisy is ready for landing (!!)\"}'},{Checked:0b,Text:'{\"text\":\"Think of cool name for company\"}'},{Checked:0b,Text:'{\"text\":\"Don\\'t crash\"}'},{Checked:0b,Text:'{\"text\":\"Ponder life\"}'},{Checked:1b,Text:'{\"text\":\"Fix wrench\"}'},{Checked:1b,Text:'{\"text\":\"Check automated milking system\"}'},{Checked:0b,Text:'{\"text\":\"Decide what to automate first\"}'}]}],PreviouslyOpenedPage:0,RepairCost:0,Type:1,display:{Name:'{\"text\":\"Day 931 - Landing!\",\"italic\":\"false\"}'}}"))
             event.player.getPersistentData().putBoolean('starter', true)
-            event.player.getPersistentData().putBoolean('new_casing_compensated', true)
+            event.player.getPersistentData().putBoolean('new_trade_engine_comp', true)
         }
-        if (!event.player.getPersistentData().getBoolean('new_casing_compensated'))
+        if (!event.player.getPersistentData().getBoolean('new_trade_engine_comp'))
         {
-            event.player.give(Item.of('ptdye:cobblestone_casing', 8))
-            event.player.give(Item.of('ptdye:redstone_casing', 12))
-            event.player.getPersistentData().putBoolean('new_casing_compensated', true)
+            event.player.getPersistentData().putBoolean('new_trade_engine_comp', true)
+            global.starterDeals.forEach(element => {
+                event.player.give(element.item) 
+            });
         }
     })
 }
