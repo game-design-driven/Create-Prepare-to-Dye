df137b32d22b85dbee7cb85f3e78515f4a6fe0f1
Yarden Zamir <dev@yarden-zamir.com>
2023-12-09 23:10:07 +0200

    feat: compensate existing players with devices

--- kubejs/server_scripts/base/featrues/playerLogin.js
+++ kubejs/server_scripts/base/featrues/playerLogin.js
@@ -30,7 +30,7 @@ if (feature('starter items')) {
             event.player.give(Item.of('create:wrench', 1))
             event.player.give(Item.of('create:clipboard', "{Pages:[{Entries:[{Checked:1b,Text:'{\"text\":\"Pet Betsy\"}'},{Checked:1b,Text:'{\"text\":\"Engine safety checks\"}'},{Checked:0b,Text:'{\"text\":\"Landing systems safety checks\"}'},{Checked:1b,Text:'{\"text\":\"Make sure Pure Daisy is ready for landing (!!)\"}'},{Checked:0b,Text:'{\"text\":\"Don\\'t crash\"}'},{Checked:0b,Text:'{\"text\":\"Ponder life\"}'},{Checked:1b,Text:'{\"text\":\"Fix wrench\"}'},{Checked:1b,Text:'{\"text\":\"Check automated milking system\"}'},{Checked:0b,Text:'{\"text\":\"Decide what to automate first\"}'}]}],PreviouslyOpenedPage:0,RepairCost:0,Type:1,display:{Name:'{\"text\":\"Day 931 - Landing!\",\"italic\":\"false\"}'}}"))
             event.player.getPersistentData().putBoolean('starter', true)
-            event.player.getPersistentData().putBoolean('new_trade_engine_comp', true)
+            event.player.getPersistentData().putBoolean('new_devices_comp', true)
 
             if (event.server.getPersistentData().getBoolean('existing_world'))
             {
@@ -40,11 +40,12 @@ if (feature('starter items')) {
             }
             event.server.getPersistentData().putBoolean('existing_world', true)
         }
-        if (!event.player.getPersistentData().getBoolean('new_trade_engine_comp'))
+        if (!event.player.getPersistentData().getBoolean('new_devices_comp'))
         {
-            event.player.getPersistentData().putBoolean('new_trade_engine_comp', true)
-            global.starterDeals.forEach(element => {
-                event.player.give(element.item) 
+            event.player.getPersistentData().putBoolean('new_devices_comp', true)
+            let devices =['8x ptdye:sealed_device', '12x ptdye:logic_device', '8x ptdye:smart_device', '8x ptdye:mechanical_device', '8x ptdye:sturdy_device']
+            devices.forEach(element => {
+                event.player.give(element) 
             });
         }
     })
