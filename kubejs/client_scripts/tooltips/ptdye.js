2ad911310ad320797c145e4dd927900972478465
TulkinRB <tulkinrb@gmail.com>
2023-12-10 17:03:22 +0200

    fix: Add missing info about immovable trading platforms to the tooltip

--- kubejs/client_scripts/tooltips/ptdye.js
+++ kubejs/client_scripts/tooltips/ptdye.js
@@ -1,4 +1,4 @@
 addTooltip(
   "ptdye:trading_transceiver",
-  "A single-use transceiver used to request an _Oscar-Delta™_ class Trading Platform® for all your inter-union trading needs. _Right click_ to request a trading platform near your current location. Expect a delivery time of up to 2-3 business days. Terms and conditions may apply."
+  "A single-use transceiver used to request an _Oscar-Delta™_ class Trading Platform® for all your inter-union trading needs. _Right click_ to request a trading platform near your current location. Expect a delivery time of up to 2-3 business days. Terms and conditions may apply. Note: The trading platform _cannot be moved_ after it lands."
 );
