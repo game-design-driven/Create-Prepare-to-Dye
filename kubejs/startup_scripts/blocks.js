96b0552375138c8b8cb90ece72c054166a284ce2
chipsams <77809761+chipsams@users.noreply.github.com>
2023-12-12 18:20:22 +0000

    feat: respritings, and transitional items! (#398)

--- kubejs/startup_scripts/blocks.js
+++ kubejs/startup_scripts/blocks.js
@@ -27,95 +27,58 @@ StartupEvents.registry("item", (event) => {
         .fastToEat() //Like dried kelp
         .meat(); //Dogs are willing to eat it
     });
+
+    event.create("ptdye:incomplete_sturdy_device", 'create:sequenced_assembly');
+    event.create("ptdye:incomplete_mechanical_device", 'create:sequenced_assembly');
+    event.create("ptdye:incomplete_sealed_device", 'create:sequenced_assembly');
+    event.create("ptdye:incomplete_smart_device", 'create:sequenced_assembly');
+    event.create("ptdye:incomplete_locomotive_device", 'create:sequenced_assembly');
+    event.create("ptdye:incomplete_logic_device", 'create:sequenced_assembly');
 });
 
 StartupEvents.registry("block", (event) => {
-  event
-    .create("ptdye:sturdy_device")
+  const device = name => {
+    return event
+      .create(name,"cardinal")
+      .hardness(2)
+      .resistance(6)
+      .tagBlock("create:wrench_pickup")
+      .opaque(false)
+      .notSolid()
+      .redstoneConductor(false)
+      .fullBlock(false)
+      .noValidSpawns(true)
+      .suffocating(false)
+      .viewBlocking(false)
+      .transparent(true);
+  }
+
+  device("ptdye:sturdy_device")
+    .material("stone")
+    .soundType("stone");
+
+  device("ptdye:logic_device")
     .material("stone")
     .soundType("stone")
-    .hardness(2)
-    .resistance(6)
-    .tagBlock("create:wrench_pickup")
-    .opaque(false)
-    .notSolid()
-    .redstoneConductor(false)
-    .fullBlock(false)
-    .noValidSpawns(true)
-    .suffocating(false);
-
-  event
-    .create("ptdye:logic_device")
-    .material("stone")
-    .soundType("stone")
-    .hardness(2)
-    .resistance(6)
-    .tagBlock("create:wrench_pickup")
-    .opaque(false)
-    .notSolid()
-    .redstoneConductor(false)
-    .fullBlock(false)
-    .noValidSpawns(true)
-    .suffocating(false)
-    .viewBlocking(false)
-    .transparent(true)
-    .box(0, 0, 0, 16, 4, 16);
-
-  event
-    .create("ptdye:sealed_device")
+    .box(0, 0, 0, 16, 2, 16);
+    
+  device("ptdye:sealed_device")
     .material("metal")
     .soundType("copper")
-    .hardness(2)
-    .resistance(6)
-    .tagBlock("create:wrench_pickup")
-    .opaque(false)
-    .notSolid()
-    .redstoneConductor(false)
-    .fullBlock(false)
-    .noValidSpawns(true)
-    .suffocating(false)
-    .viewBlocking(false)
-    .transparent(true)
     .box(0, 0, 0, 16, 15, 16);
 
-  event
-    .create("ptdye:smart_device")
+  device("ptdye:smart_device")
+    .material("metal")
+    .soundType("metal");
+    
+
+  device("ptdye:mechanical_device")
+    .material("metal")
+    .soundType("wood");
+
+  device("ptdye:locomotive_device")
     .material("metal")
     .soundType("metal")
-    .hardness(2)
-    .resistance(6)
-    .tagBlock("create:wrench_pickup")
-    .opaque(false)
-    .notSolid()
-    .redstoneConductor(false)
-    .fullBlock(false)
-    .noValidSpawns(true)
-    .suffocating(false)
-    .viewBlocking(false)
-    .transparent(true);
-
-  event
-    .create("ptdye:mechanical_device")
-    .material("metal")
-    .soundType("wood")
-    .hardness(2)
-    .resistance(6)
-    .tagBlock("create:wrench_pickup")
-    .opaque(false)
-    .notSolid()
-    .redstoneConductor(false)
-    .fullBlock(false)
-    .noValidSpawns(true)
-    .suffocating(false)
-    .viewBlocking(false)
-    .transparent(true);
-
-  event
-    .create("ptdye:locomotive_device")
-    .textureAll("create:block/railway_casing")
-    .material("metal")
-    .soundType("metal")
-    .hardness(2)
-    .resistance(6)
-    .tagBlock("create:wrench_pickup");
+    .box(3, 0, 0, 13, 9, 16)
+    .box(4, 9, 4, 12, 12, 16);
 });
