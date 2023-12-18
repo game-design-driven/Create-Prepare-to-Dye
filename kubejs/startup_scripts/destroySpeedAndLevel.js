b9072ba548a59382afc2ebc47d21f4b372df9416
Yarden Zamir <dev@yarden-zamir.com>
2023-12-18 21:34:59 +0200

    feat: increase break speed of gray plate

--- kubejs/startup_scripts/destroySpeedAndLevel.js
+++ kubejs/startup_scripts/destroySpeedAndLevel.js
@@ -4,7 +4,7 @@ BlockEvents.modification(e => {
         block.destroySpeed = Block.getBlock(block.id).defaultDestroyTime() / 11
     })
     e.modify('/^quark:iron_plate.*/', block => {
-        block.destroySpeed = Block.getBlock(block.id).defaultDestroyTime() / 22
+        block.destroySpeed = Block.getBlock(block.id).defaultDestroyTime() / 30
     })    
 })
 
