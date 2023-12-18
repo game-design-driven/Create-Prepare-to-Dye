ad2e08cfe62d2c951458275845367362cfc00365
Yarden Zamir <dev@yarden-zamir.com>
2023-12-18 11:45:57 +0200

    fix: errors in ingot replacement file

--- kubejs/server_scripts/base/featrues/ingotReplacement.js
+++ kubejs/server_scripts/base/featrues/ingotReplacement.js
@@ -12,29 +12,27 @@ if (feature('Gold is 4 nuggets')) {
   addShapeless('4x minecraft:gold_nugget', 'gold_ingot')
   
   removeRecipe({id: "create:crushing/nether_gold_ore"})
-  addCrushing(['8x gold_nugget', 'create:experience_nugget %75', 'netherrack %10'])
+  addCrushing(['8x gold_nugget', 'create:experience_nugget %75', 'netherrack %10'], 'nether_gold_ore')
 
 }
 
 //this bit is still being bad! i don't know why!!
-/*
-if(feature('No More Ingots')){
+// if(feature('No More Ingots')){
 
-  //this went over relatively smoothly
-  removeAndReplace("minecraft:iron_ingot","create:iron_sheet");
+//   //this went over relatively smoothly
+//   removeAndReplace("minecraft:iron_ingot","create:iron_sheet");
 
-  removeRecipe({input:"create:iron_sheet",output:"create:iron_sheet"});
-  addPressing("create:iron_sheet","minecraft:raw_iron");
-  addShapeless("create:iron_sheet",["9x #forge:nuggets/iron"])
+//   removeRecipe({input:"create:iron_sheet",output:"create:iron_sheet"});
+//   addPressing("create:iron_sheet","minecraft:raw_iron");
+//   addShapeless("create:iron_sheet",["9x #forge:nuggets/iron"])
 
   
-  //for some reason two remove and replaces seems to get rid of copper tubes...
-  removeAndReplace("minecraft:copper_ingot","ptdye:copper_tubes");
-  replaceInputForRecipes("create:copper_sheet","ptdye:copper_tubes");
+//   //for some reason two remove and replaces seems to get rid of copper tubes...
+//   removeAndReplace("minecraft:copper_ingot","ptdye:copper_tubes");
+//   replaceInputForRecipes("create:copper_sheet","ptdye:copper_tubes");
   
-  //TODO: this needs ballencing!!!! i just felt like it didn't make sense to have it be processed in a press anymore, and it's a very fluid-oriented material.
-  addMixing(["ptdye:orange_tubes",Item.of("minecraft:clay").withChance(0.5)],"create:crushed_raw_copper","NONE",300,Fluid.of("minecraft:water").withAmount(10),Fluid.of("minecraft:milk").withAmount(10))
+//   //TODO: this needs ballencing!!!! i just felt like it didn't make sense to have it be processed in a press anymore, and it's a very fluid-oriented material.
+//   addMixing(["ptdye:orange_tubes",Item.of("minecraft:clay").withChance(0.5)],"create:crushed_raw_copper","NONE",300,Fluid.of("minecraft:water").withAmount(10),Fluid.of("minecraft:milk").withAmount(10))
   
-  removeAndReplace("minecraft:copper_block","ptdye:orange_tube_block");
-}
-//*/
+//   removeAndReplace("minecraft:copper_block","ptdye:orange_tube_block");
+// }
