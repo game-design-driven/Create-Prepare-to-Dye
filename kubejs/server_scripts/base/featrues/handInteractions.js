481c3e41444ce7889d2b03dd7d31a20d5c73121d
Yarden Zamir <dev@yarden-zamir.com>
2023-12-18 21:34:19 +0200

    feat: spouts can be filled with fluids that have block form

--- kubejs/server_scripts/base/featrues/handInteractions.js
+++ kubejs/server_scripts/base/featrues/handInteractions.js
@@ -1,4 +1,41 @@
+function fillBlockEntity(block, event) {
+
+  let fluid;
+  let amount = 1000;
+  if (event.player.handSlots[0].id == "minecraft:milk_bucket") {
+    fluid = "minecraft:milk";
+  } else {
+    fluid = event.player.handSlots[0].id.replace("_bucket", "");
+  }
+  if (!event.player.isCreative()) {
+    event.player.setMainHandItem("minecraft:bucket");
+  }
+  event.level.runCommandSilent(
+    `playsound minecraft:item.bucket.empty block @a ${event.block.x} ${event.block.y} ${event.block.z} 0.4 1.7`
+  );
+  block.mergeEntityData({
+    Tanks: [
+      {
+        TankContent: {
+          Amount: amount,
+          FluidName: fluid,
+        },
+      },
+    ],
+  });
+}
+
 BlockEvents.rightClicked("createdieselgenerators:diesel_engine", (event) => {
+  if (
+
+    (event.player.handSlots[0].id.endsWith('_bucket')) &&
+    event.block.getEntityData().Tanks[0].Level.Value == 0
+  ) {
+    fillBlockEntity(event.block, event)
+    event.cancel();
+  }
+});
+BlockEvents.rightClicked("create:spout", (event) => {
   if (
     (event.player.handSlots[0].id == "minecraft:milk_bucket" ||
     event.player.handSlots[0].id == "kubejs:fermented_goop_bucket" ||
@@ -6,29 +43,13 @@ BlockEvents.rightClicked("createdieselgenerators:diesel_engine", (event) => {
         event.player.handSlots[0].id.endsWith("_bucket"))) &&
     event.block.getEntityData().Tanks[0].Level.Value == 0
   ) {
-    let fluid;
-    if (event.player.handSlots[0].id == "minecraft:milk_bucket") {
-      fluid = "minecraft:milk";
-    } else {
-      fluid = event.player.handSlots[0].id.replace("_bucket", "");
-    }
+    let amount = 1000;
+    let fluid = event.player.handSlots[0].id.replace("_bucket", "")
+    Utils.server.runCommand(`setblock ${event.block.x} ${event.block.y} ${event.block.z} air`);
+    Utils.server.runCommand(`setblock ${event.block.x} ${event.block.y} ${event.block.z} create:spout{Tanks:[{TankContent:{Amount:${amount},FluidName:"${fluid}"}}]}`);
     if (!event.player.isCreative()) {
       event.player.setMainHandItem("minecraft:bucket");
     }
-    event.level.runCommandSilent(
-      `playsound minecraft:item.bucket.empty block @a ${event.block.x} ${event.block.y} ${event.block.z} 0.4 1.7`
-    );
-
-    event.block.mergeEntityData({
-      Tanks: [
-        {
-          TankContent: {
-            Amount: 1000,
-            FluidName: fluid,
-          },
-        },
-      ],
-    });
     event.cancel();
   }
 });
