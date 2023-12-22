c19f8483473e79b999098f072f9d49d3152f9be4
Yarden Zamir <dev@yarden-zamir.com>
2023-12-22 15:14:04 +0200

    dev: format lychee

--- kubejs/server_scripts/base/recipe_managers/lychee.manager.js
+++ kubejs/server_scripts/base/recipe_managers/lychee.manager.js
@@ -1,165 +1,188 @@
 // priority: 100
 
 function addItemInside(post, input, block_in) {
-    let result = {
-        type: 'lychee:item_inside',
-        item_in: solveLimitedIngredients(input),
-        block_in: block_in,
-        post: post,
-    }
-    modpackRecipes.push(result)
+  let result = {
+    type: "lychee:item_inside",
+    item_in: solveLimitedIngredients(input),
+    block_in: block_in,
+    post: post,
+  };
+  modpackRecipes.push(result);
 }
 function addItemInsidePlace(block_out, input, block_in) {
-    let result = {
-        type: 'lychee:item_inside',
-        item_in: solveLimitedIngredients(input),
-        block_in: block_in,
-        post: {
-            type: 'place',
-            block: block_out
-        },
-    }
-    modpackRecipes.push(result)
+  let result = {
+    type: "lychee:item_inside",
+    item_in: solveLimitedIngredients(input),
+    block_in: block_in,
+    post: {
+      type: "place",
+      block: block_out,
+    },
+  };
+  modpackRecipes.push(result);
 }
 
 function addDripping(block_out, target_block, source_block) {
-    if (!source_block) source_block = 'water'
-    let result = {
-        type: "lychee:dripstone_dripping",
-        source_block: source_block,
-        target_block: target_block,
-        post: [
-            {
-                type: "place",
-                block: block_out
-            }
-        ]
-    }
-    modpackRecipes.push(result)
+  if (!source_block) source_block = "water";
+  let result = {
+    type: "lychee:dripstone_dripping",
+    source_block: source_block,
+    target_block: target_block,
+    post: [
+      {
+        type: "place",
+        block: block_out,
+      },
+    ],
+  };
+  modpackRecipes.push(result);
 }
 function addBlockInteract(block_out, block_in, item_in) {
-    let post = {
-        type: 'place',
-        block: block_out,
-    }
-    let recipe = {
-        type: 'lychee:block_interacting',
-        item_in: solveLimitedIngredient(item_in),
-        block_in: block_in,
-        post: post,
-    }
-    modpackRecipes.push(recipe)
+  let post = {
+    type: "place",
+    block: block_out,
+  };
+  let recipe = {
+    type: "lychee:block_interacting",
+    item_in: solveLimitedIngredient(item_in),
+    block_in: block_in,
+    post: post,
+  };
+  modpackRecipes.push(recipe);
 }
 function addBlockExplode(block_out, block_in) {
-    let post = {
-        type: 'place',
-        block: block_out,
-    }
-    let recipe = {
-        type: 'lychee:block_exploding',
-        block_in: block_in,
-        post: post,
-    }
-    modpackRecipes.push(recipe)
+  let post = {
+    type: "place",
+    block: block_out,
+  };
+  let recipe = {
+    type: "lychee:block_exploding",
+    block_in: block_in,
+    post: post,
+  };
+  modpackRecipes.push(recipe);
 }
-const advancementRadius = 50
+const advancementRadius = 50;
 function addBlockInteractToItem(item_out, block_in, item_in, advancement) {
-    let post = [
-        {
-            type: "place",
-            block: 'air'
-        },
-        {
-        type: 'drop_item',
-        item: item_out,
+  let post = [
+    {
+      type: "place",
+      block: "air",
     },
-
-]
-    if (advancement != null) {
-        post[2] = {
-            type: 'execute',
-            hide: true,
-            command: `advancement grant @a[dx=${advancementRadius},dy=${advancementRadius},dz=${advancementRadius}] until ${advancement}`
-        }
-    }
-    let recipe = {
-        type: 'lychee:block_interacting',
-        item_in: solveLimitedIngredient(item_in),
-        block_in: block_in,
-        post: post,
-    }
-    modpackRecipes.push(recipe)
+    {
+      type: "execute",
+      command: `summon item ~ ~1 ~ {Item:{id:"${Item.of(item_out).id}",Count:${
+        Item.of(item_out).count
+      }}}`,
+    },
+  ];
+  if (advancement != null) {
+    post[2] = {
+      type: "execute",
+      hide: true,
+      command: `advancement grant @a[dx=${advancementRadius},dy=${advancementRadius},dz=${advancementRadius}] until ${advancement}`,
+    };
+  }
+  let recipe = {
+    type: "lychee:block_interacting",
+    item_in: solveLimitedIngredient(item_in),
+    block_in: block_in,
+    post: post,
+  };
+  modpackRecipes.push(recipe);
 }
 function addGrow(block_out, block_in, item_in, ghost) {
-    ghost = ghost || false
-    let post = [{
-        type: 'place',
-        block: block_out,
-        offsetY: 1,
+  ghost = ghost || false;
+  let post = [
+    {
+      type: "place",
+      block: block_out,
+      offsetY: 1,
     },
     {
-        "type": "execute",
-        "command": "particle minecraft:happy_villager ~ ~1 ~ 1 1 1 0 5",
-        "hide": true
+      type: "execute",
+      command: "particle minecraft:happy_villager ~ ~1 ~ 1 1 1 0 5",
+      hide: true,
     },
     {
-        "type": "execute",
-        "command": "playsound minecraft:item.bone_meal.use block @r ~ ~1 ~",
-        "hide": true
-    }]
-    let recipe = {
-        type: 'lychee:block_interacting',
-        item_in: solveLimitedIngredient(item_in),
-        block_in: block_in,
-        post: post,
-        ghost: ghost,
-    }
-    modpackRecipes.push(recipe)
+      type: "execute",
+      command: "playsound minecraft:item.bone_meal.use block @r ~ ~1 ~",
+      hide: true,
+    },
+  ];
+  let recipe = {
+    type: "lychee:block_interacting",
+    item_in: solveLimitedIngredient(item_in),
+    block_in: block_in,
+    post: post,
+    ghost: ghost,
+  };
+  modpackRecipes.push(recipe);
 }
-addItemInside([
-    {type: 'drop_item', item: 'white_dye'},
-    {type: 'place', block: {blocks: ['water_cauldron'], state: {level: 2}}},
-], '#forge:dyes', { blocks: ['water_cauldron'],level: 3, state: {}})
-
-addItemInside([
+addItemInside(
+  [
+    { type: "drop_item", item: "white_dye" },
     {
-        type: 'drop_item',
-        item: 'white_dye'
+      type: "place",
+      block: { blocks: ["water_cauldron"], state: { level: 2 } },
     },
-    {type: 'place', block: {blocks: ['water_cauldron'], state: {level: 1}}},
-], '#forge:dyes', {
-    blocks: ['water_cauldron'],
-    state: {
-        level: 2
-    }
-})
+  ],
+  "#forge:dyes",
+  { blocks: ["water_cauldron"], level: 3, state: {} }
+);
 
-addItemInside([
+addItemInside(
+  [
     {
-        type: 'drop_item',
-        item: 'white_dye'
+      type: "drop_item",
+      item: "white_dye",
     },
     {
-        type: 'place',
-        block: 'cauldron'
-    }
-], '#forge:dyes', {
-    blocks: ['water_cauldron'],
+      type: "place",
+      block: { blocks: ["water_cauldron"], state: { level: 1 } },
+    },
+  ],
+  "#forge:dyes",
+  {
+    blocks: ["water_cauldron"],
     state: {
-        level: 1
-    }
-})
+      level: 2,
+    },
+  }
+);
 
-addItemInside([
+addItemInside(
+  [
     {
-        type: 'drop_item',
-        item: 'white_dye'
-    }
-], '#forge:dyes', {
-    blocks: ['water'],
-})
+      type: "drop_item",
+      item: "white_dye",
+    },
+    {
+      type: "place",
+      block: "cauldron",
+    },
+  ],
+  "#forge:dyes",
+  {
+    blocks: ["water_cauldron"],
+    state: {
+      level: 1,
+    },
+  }
+);
 
-
+addItemInside(
+  [
+    {
+      type: "drop_item",
+      item: "white_dye",
+    },
+  ],
+  "#forge:dyes",
+  {
+    blocks: ["water"],
+  }
+);
 
 // {
 //     "type": "lychee:item_inside",
