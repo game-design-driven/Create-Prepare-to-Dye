b16e8ebc2b40e81e3b664db0277099c4b618f7db
Yarden Zamir <dev@yarden-zamir.com>
2023-12-22 12:29:25 +0200

    feat: add features.json to let players disable features we added. No garentees for how the game will mesh after that thoguh

--- kubejs/server_scripts/base/features.js
+++ kubejs/server_scripts/base/features.js
@@ -1,10 +1,20 @@
-//priority:100
+// priority: 100
+
+//read features.json
+console.log("Reading features.json and indexing all feature rules");
+let featuresUserSettings = JsonIO.read(
+  "kubejs/features.json"
+)
+featuresUserSettings.forEach((key, value) =>{
+    global.features.set(key,value)
+})
 function feature(name) {
-    return global.feature(name)
-}
-function disableFeature(name) {
-    global.disableFeature(name)
-}
-function enableFeature(name) { //don't see why you do this as they are all enabled by default
-    global.enableFeature(name, true)
+    if (global.features.get(name)){
+        return global.features.get(name)
+    }
+    else{
+        global.features.set(name, true)
+        console.log('added missing feature '+name)
+        return true
+    }
 }
