0a375c50ba6acdf6a0dd8917563f8abf1f4bcdfc
Yarden Zamir <dev@yarden-zamir.com>
2023-12-22 09:52:22 +0200

    dev: add debug tools script for devs

---
+++ kubejs/server_scripts/base/debugtools.js
@@ -0,0 +1,19 @@
+// Priority: 1
+const $UUIDUtil = Java.loadClass("net.minecraft.core.UUIDUtil");
+ServerEvents.commandRegistry((event) => {
+    const {
+        commands: Commands,
+        arguments: Arguments,
+        builtinSuggestions: Suggestions,
+      } = event;
+  event.register(
+    Commands.literal("uuid").executes((context) => {
+      let player = context.getSource().getPlayer();
+      let playerUUID = player.uuid;
+      let intArrays = $UUIDUtil.uuidToIntArray(playerUUID);
+      player.tell(intArrays);
+      return 0;
+    })
+  );
+});
+
