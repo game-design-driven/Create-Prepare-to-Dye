052bb6450ba84491e3606a325071edaaa6bedb35
Yarden Zamir <dev@yarden-zamir.com>
2023-12-22 09:31:25 +0200

    dev: format process.js file

--- kubejs/server_scripts/base/process.js
+++ kubejs/server_scripts/base/process.js
@@ -1,20 +1,20 @@
 // priority: -100
-ServerEvents.recipes(event => {
-  modpackRecipes.forEach(recipe => {
-    removeAirFromRecipe(recipe)
+ServerEvents.recipes((event) => {
+  modpackRecipes.forEach((recipe) => {
+    removeAirFromRecipe(recipe);
     if (!hasRemovedItems(recipe)) {
-        let r = event.custom(recipe)
-        r.id = getUniqueRecipeName(r)
-        if (recipe.hidden) {
-            r.id = r.id+"/hidden"
-        }
+      let r = event.custom(recipe);
+      r.id = getUniqueRecipeName(r);
+      if (recipe.hidden) {
+        r.id = r.id + "/hidden";
+      }
     }
   });
-  
-  modpackRemovedRecipes.forEach(recipeFilter => {
-      event.remove(recipeFilter)
+
+  modpackRemovedRecipes.forEach((recipeFilter) => {
+    event.remove(recipeFilter);
   });
-})
+});
 
 // onEvent('server.datapack.low_priority', (event) => {
 //     event.addJson('botania:orechid_ore_weights/orechid.json', {
@@ -26,28 +26,28 @@ ServerEvents.recipes(event => {
 //what's wrong with you. you've wasted so much of people's limited time on earth.
 let lazy_removedRecipesMap = null;
 
-function createRemovedRecipeMap(itemsToRemove){
-let map = {};
-for(let item of itemsToRemove){
-  map[item] = true;
-}
-return map
+function createRemovedRecipeMap(itemsToRemove) {
+  let map = {};
+  for (let item of itemsToRemove) {
+    map[item] = true;
+  }
+  return map;
 }
 
 function hasRemovedItems(recipe) {
-  if(!lazy_removedRecipesMap) lazy_removedRecipesMap = createRemovedRecipeMap(global.itemsToRemove);
+  if (!lazy_removedRecipesMap)
+    lazy_removedRecipesMap = createRemovedRecipeMap(global.itemsToRemove);
   if (Array.isArray(recipe.ingredients)) {
-      for(let ingredient of recipe.ingredients) {
-          if (lazy_removedRecipesMap[ingredient]) {
-            return true
-          }
+    for (let ingredient of recipe.ingredients) {
+      if (lazy_removedRecipesMap[ingredient]) {
+        return true;
       }
+    }
   }
   if (recipe.output) {
-      if (lazy_removedRecipesMap[recipe.output]) {
-          
-          return true
-      }
+    if (lazy_removedRecipesMap[recipe.output]) {
+      return true;
+    }
   }
-  return false
+  return false;
 }
