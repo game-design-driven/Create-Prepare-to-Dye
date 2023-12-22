f137e8805eef014b3ecc02c9cd39653a5cc41de6
Yarden Zamir <dev@yarden-zamir.com>
2023-12-22 09:19:23 +0200

    fix: plank sawing only works for acacia. Now acacia gives more but all the others do work

--- kubejs/server_scripts/base/featrues/trees.js
+++ kubejs/server_scripts/base/featrues/trees.js
@@ -1,9 +1,9 @@
 //making wood great again (any type of wood should have a reason to exist)
-if (feature('Oak aka applewood')) {
-    //oak is turned into apple tree by name as it's already in function
-    //should change lag files
-    addMilling('apple %20', 'oak_leaves')
-    addCrushing(['apple %5', 'stick %5', 'gray_dye %5'], 'oak_leaves')
+if (feature("Oak aka applewood")) {
+  //oak is turned into apple tree by name as it's already in function
+  //should change lag files
+  addMilling("apple %20", "oak_leaves");
+  addCrushing(["apple %5", "stick %5", "gray_dye %5"], "oak_leaves");
 }
 // if (feature('dark oak')) {
 //     addAssembly(['8x paper %8', 'brown_dye', 'stick'], 'dark_oak_log', [
@@ -11,19 +11,32 @@ if (feature('Oak aka applewood')) {
 //         addPressing()
 //     ], 2)
 // }
-if (feature('Acacia')) {
-    removeRecipe({ id: 'minecraft:stick' })
-    addShaped('4x stick', ['w', 'w'], { w: '#planks' })
-    addShaped('6x stick', ['w', 'w'], { w: 'acacia_planks' })
-    addCutting('3x stick', 'acacia_planks')
+if (feature("Acacia")) {
+  removeRecipe({ id: "minecraft:stick" });
+  addShaped("4x stick", ["w", "w"], { w: "#planks" });
+  addShaped("6x stick", ["w", "w"], { w: "acacia_planks" });
+  addCutting("3x stick", "acacia_planks");
 }
-if (feature('Spruce')) {
-    removeRecipe({ id: 'minecraft:charcoal' })
-    addSmelting('charcoal', '#logs_that_burn')
-    addSmelting('2x charcoal', 'spruce_log', undefined, 800)
+if (feature("Spruce")) {
+  removeRecipe({ id: "minecraft:charcoal" });
+  addSmelting("charcoal", "#logs_that_burn");
+  addSmelting("2x charcoal", "spruce_log", undefined, 800);
 }
-if (feature('Birch')) {
-    removeRecipe({ id: 'minecraft:birch_planks' })
-    addShapeless('5x birch_planks', 'birch_log')
-    addSmelting('charcoal', 'birch_log', undefined, 50)
+if (feature("Birch")) {
+  removeRecipe({ id: "minecraft:birch_planks" });
+  addShapeless("5x birch_planks", "birch_log");
+  addSmelting("charcoal", "birch_log", undefined, 50);
+}
+
+if (feature("Cut any plank to 2 sticks")) {
+  //this might not be needed in future
+  addCutting("2x stick", Ingredient.of([
+    "oak_planks",
+    "spruce_planks",
+    "birch_planks",
+    "jungle_planks",
+    "dark_oak_planks",
+    "crimson_planks",
+    "warped_planks",
+  ]));
 }
