8a72968d8615465c2cfce1e18189eba74e73b1a3
Jasper Alani <jasperalani@gmail.com>
2023-12-23 23:30:12 +0000

    feat: make all glass types drop item
    make all types of glass drop either itself or the base variant item

--- kubejs/server_scripts/base/loot.js
+++ kubejs/server_scripts/base/loot.js
@@ -1,4 +1,16 @@
 LootJS.modifiers((event) => {
     event.addBlockLootModifier("#minecraft:leaves").removeLoot("minecraft:stick");
     event.addBlockLootModifier("#minecraft:leaves").removeLoot("minecraft:apple");
+
+    // Glass
+    event.addBlockLootModifier("minecraft:glass").addLoot("minecraft:glass");
+    event.addBlockLootModifier("minecraft:glass_pane").addLoot("minecraft:glass_pane");
+    event.addBlockLootModifier("minecraft:white_stained_glass").addLoot("minecraft:white_stained_glass");
+    event.addBlockLootModifier("minecraft:white_stained_glass_pane").addLoot("minecraft:white_stained_glass_pane");
+    event.addBlockLootModifier("create:framed_glass").addLoot("create:framed_glass");
+    event.addBlockLootModifier("create:horizontal_framed_glass").addLoot("create:horizontal_framed_glass");
+    event.addBlockLootModifier("create:vertical_framed_glass").addLoot("create:vertical_framed_glass");
+    event.addBlockLootModifier("create:tiled_glass_pane").addLoot("create:tiled_glass_pane");
+    event.addBlockLootModifier("create:framed_glass_pane").addLoot("create:framed_glass_pane");
+    event.addBlockLootModifier("create:horizontal_framed_glass_pane").addLoot("create:horizontal_framed_glass_pane");
 });
