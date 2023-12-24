9069f9122f268ac711cc58be50f9fe1e01e53595
Yarden Zamir <dev@yarden-zamir.com>
2023-12-24 18:58:42 +0200

    tweak: Add stripping recipes to logs that dont have them like livingwood log to make alternative wood aquisition methods more clear

--- kubejs/server_scripts/base/featrues/wood.js
+++ kubejs/server_scripts/base/featrues/wood.js
@@ -9,3 +9,10 @@ if (feature('Replace log>wood recipes with manapool recipes')) {
     });
     
 }
+if (feature('Add stripping recipes to logs that dont have them')) {
+    addItemApplication('botania:stripped_livingwood_log','botania:livingwood_log', '#forge:tools/axes')
+    addItemApplication('botania:glimmering_stripped_livingwood_log','botania:glimmering_livingwood_log', '#forge:tools/axes')
+    addItemApplication('botania:stripped_dreamwood_log','botania:dreamwood_log', '#forge:tools/axes')
+    addItemApplication('botania:glimmering_stripped_dreamwood_log','botania:glimmering_dreamwood_log', '#forge:tools/axes')
+    addItemApplication('quark:stripped_blossom_log','quark:blossom_log', '#forge:tools/axes')
+}
