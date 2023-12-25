cf11cff5a75779798c928670c2f4aeec54819fe3
Yarden Zamir <dev@yarden-zamir.com>
2023-12-25 16:13:23 +0200

    feat: quark variants are now assembler variants and can be crafter bi-directionally

--- kubejs/server_scripts/base/featrues/variants.js
+++ kubejs/server_scripts/base/featrues/variants.js
@@ -2,5 +2,6 @@ if (feature('Remove and add stone cutting for variant recipes')) {
     global.variants.forEach(variant => {
         removeRecipe({output: variant[0]})
         addStonecutting(variant[0],variant[1])
+        addStonecutting(variant[1],variant[0])
     });
 }
