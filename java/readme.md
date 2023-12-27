8ec5879f52cf6d69249be632e94339de23f9e2c4
Jasper Alani <jasperalani@gmail.com>
2023-12-27 02:29:48 +0000

    dev: move ptdye-plus to new repo

--- java/readme.md
+++
@@ -1,5 +0,0 @@
-### Ptdye Plus
-
-A Java mod for the Create: Prepare to Dye modpack
-
-This mod contains code relating to features/bugfixes that are not possible to implement solely with KubeJS.
--- java/src/main/java/com/kikis/ptdyeplus/MinecraftMenuBuilder.java
+++
@@ -1,9 +0,0 @@
-package com.kikis.ptdyeplus;
-
-import net.minecraft.world.entity.player.Inventory;
-import net.minecraft.world.inventory.AbstractContainerMenu;
-import net.minecraft.world.inventory.ContainerLevelAccess;
-
-interface MinecraftMenuBuilder {
-	AbstractContainerMenu create(int id, Inventory inventory, ContainerLevelAccess access);
-}
