e3d1e015d68d4a46c40c63846ef0c5c1b165b652
redcatone <24564581+redcatone@users.noreply.github.com>
2023-12-28 17:48:10 -0500

    Wrap feature and use seed tag

--- kubejs/server_scripts/base/featrues/chickenEggs.js
+++ kubejs/server_scripts/base/featrues/chickenEggs.js
@@ -1,31 +1,30 @@
-// Change chicken egg generation from natural generation to being fed a seed
-
-// Set egg cooldown to max on spawn
-EntityEvents.spawned((event) => {
-  if (event.getEntity().getType() == "minecraft:chicken") {
-    event.getEntity().mergeNbt({ "EggLayTime": 2147483647 })
-  }
-});
-
-// Spawn an egg when a chicken is fed a seed
-ItemEvents.entityInteracted((event) => {
-  if (event.getTarget().getType() == "minecraft:chicken") {
-    let seeds = ["minecraft:wheat_seeds", "minecraft:melon_seeds", "minecraft:pumpkin_seeds", "minecraft:beetroot_seeds"];
-    if (seeds.includes(event.getItem().getId())) {
-      let egg = event.level.createEntity("minecraft:item");
-      egg.item = "minecraft:egg";
-      egg.x = event.getTarget().x;
-      egg.y = event.getTarget().y + 0.2;
-      egg.z = event.getTarget().z;
-      egg.setPickUpDelay(20);
-      egg.setMotion(((-0.5 + Math.random()) / 8), 0.2, ((-0.5 + Math.random()) / 8))
-      egg.spawn();
-
-      event.server.runCommandSilent(`playsound minecraft:entity.chicken.egg neutral @a ${event.getTarget().x} ${event.getTarget().y} ${event.getTarget().z} 5`);
-      event.player.swing();
-      event.getItem().count--;
+if (feature("Change chicken egg laying from natural generation to being fed a seed")) {
+  // Disable natural egg laying
+  EntityEvents.spawned((event) => {
+    if (event.getEntity().getType() == "minecraft:chicken") {
+      event.getEntity().mergeNbt({ "EggLayTime": 2147483647 })
     }
-    // Disable breeding
-    event.cancel();
-  }
-});
+  });
+
+  // Spawn an egg when a chicken is fed a seed
+  ItemEvents.entityInteracted((event) => {
+    if (event.getTarget().getType() == "minecraft:chicken") {
+      if (event.getItem().hasTag("forge:seeds")) {
+        let egg = event.level.createEntity("minecraft:item");
+        egg.item = "minecraft:egg";
+        egg.x = event.getTarget().x;
+        egg.y = event.getTarget().y + 0.2;
+        egg.z = event.getTarget().z;
+        egg.setPickUpDelay(20);
+        egg.setMotion(((-0.5 + Math.random()) / 8), 0.2, ((-0.5 + Math.random()) / 8))
+        egg.spawn();
+
+        event.server.runCommandSilent(`playsound minecraft:entity.chicken.egg neutral @a ${event.getTarget().x} ${event.getTarget().y} ${event.getTarget().z} 5`);
+        event.player.swing();
+        event.getItem().count--;
+
+        event.cancel();  // Disable breeding
+      }
+    }
+  });
+}
