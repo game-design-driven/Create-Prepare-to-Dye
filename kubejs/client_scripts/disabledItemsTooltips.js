e7f8ff6813ad35d8b537f7a476572dc010191868
Yarden Zamir <dev@yarden-zamir.com>
2023-12-30 19:02:29 +0200

    feat: add tooltip for items that are disabled so that if a player sees one he can report it easily

---
+++ kubejs/client_scripts/disabledItemsTooltips.js
@@ -0,0 +1,18 @@
+ItemEvents.tooltip((event) => {
+  event.addAdvanced(Ingredient.of("#c:removed"), (item, advanced, text) => {
+    text.add(
+      Text.red(
+        `This item was removed from the game. If you acquired it in Automation mode, please contact the Create: Prepare to Dye developers on Discord or Github`
+      )
+    );
+  });
+  event.addAdvanced(Ingredient.of("%food"), (item, advanced, text) => {
+    let nutrition = item.getFoodProperties(null).getNutrition();
+    let saturationModifier = item.getFoodProperties(
+      Client.player
+    ).saturationModifier;
+    let saturation = Math.round(nutrition * saturationModifier * 2 * 10) / 10;
+    text.add(Text.darkGreen('Saturation').append(Text.green(` ${saturation}`)));
+    text.add(Text.darkGreen('Nutrition').append(Text.green(` ${nutrition}`)));
+  });
+});
