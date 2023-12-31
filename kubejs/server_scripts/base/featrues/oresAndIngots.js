0bc5a06e5e110bb8d3e5d3e2f8de5b8c734f65ae
Yarden Zamir <dev@yarden-zamir.com>
2023-12-31 11:32:52 +0200

    tweak: lapis from dust and water recipe requires less water

--- kubejs/server_scripts/base/featrues/oresAndIngots.js
+++ kubejs/server_scripts/base/featrues/oresAndIngots.js
@@ -220,7 +220,7 @@ if (feature('Lapis')) {
     //     <item:minecraft:lapis_ore>.murdurFamily();
     //     Orechid.main.removeOreWeight(<blockstate:minecraft:lapis_ore>);
     removeItem('lapis_ore')
-    addCompacting('lapis_lazuli', '2x #forge:dusts', temperature.heated,undefined,undefined,'1000x water')
+    addCompacting('lapis_lazuli', ['2x #forge:dusts','500mb water'])
     //remove from orechid
     // addFluidTransomItem('2x lapis_lazuli', ['#forge:dyes', '#forge:lime'])
     // addFluidTransomItem('2x lapis_lazuli', '#forge:dusts/mana')
