4770224124acd6058e4144a34aa9197b4456c444
Yarden Zamir <dev@yarden-zamir.com>
2024-01-06 13:34:06 +0200

    fix: tooltip for raw white and raw orange mentioning concreat powder instead of just powder

--- kubejs/client_scripts/tooltips/dyeing.js
+++ kubejs/client_scripts/tooltips/dyeing.js
@@ -5,10 +5,10 @@ addTooltip(
 addTooltip(["iron_ore"], "Can be made by dyeing _stone_ with _white dye_");
 addTooltip(
   ["iron_ingot", "raw_iron"],
-  "Can be made by dyeing _white concrete powder_ or _stone_ with _white dye_"
+  "Can be made by dyeing _white powder_ or _stone_ with _white dye_"
 );
 addTooltip(["copper_ore"], "Can be made by dyeing _stone_ with _orange dye_");
 addTooltip(
   ["copper_ingot", "raw_copper"],
-  "Can be made by dyeing _orange concrete powder_ or _stone_ with _orange dye_"
+  "Can be made by dyeing _orange powder_ or _stone_ with _orange dye_"
 );
