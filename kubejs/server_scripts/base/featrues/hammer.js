a0218eab280caf91362948ecdd47830dfc330011
chipsams <77809761+chipsams@users.noreply.github.com>
2024-01-15 19:51:02 +0000

    make hammer work with new tagging system

--- kubejs/server_scripts/base/featrues/hammer.js
+++ kubejs/server_scripts/base/featrues/hammer.js
@@ -1,12 +1,12 @@
 const hammer_deviceMap = new Map([
-  ["forge:device/mechanical", "ptdye:mechanical_device"],
-  ["forge:device/sturdy", "ptdye:sturdy_device"],
-  ["forge:device/logic", "ptdye:logic_device"],
-  ["forge:device/sealed", "ptdye:sealed_device"],
-  ["forge:device/smart", "ptdye:smart_device"],
-  ["forge:device/locomotive", "ptdye:locomotive_device"],
-  ["forge:device/red_stringed", "ptdye:red_stringed_device"],
-  ["forge:device/furniture", "ptdye:furnished_device"],
+  ["forge:devices/mechanical", "ptdye:mechanical_device"],
+  ["forge:devices/sturdy", "ptdye:sturdy_device"],
+  ["forge:devices/logic", "ptdye:logic_device"],
+  ["forge:devices/sealed", "ptdye:sealed_device"],
+  ["forge:devices/smart", "ptdye:smart_device"],
+  ["forge:devices/locomotive", "ptdye:locomotive_device"],
+  ["forge:devices/red_stringed", "ptdye:red_stringed_device"],
+  ["forge:devices/furniture", "ptdye:furnished_device"],
 ]);
 
 function hammer_breakBlock(block, item, player){
