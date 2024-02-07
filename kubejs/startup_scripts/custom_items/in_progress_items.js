b984710adb09c0d321ce23003ff1a72e396289e4
Yarden Zamir <dev@yarden-zamir.com>
2024-02-07 22:06:45 +0200

    tweak: add textures for pizza

--- kubejs/startup_scripts/custom_items/in_progress_items.js
+++ kubejs/startup_scripts/custom_items/in_progress_items.js
@@ -1,4 +1,5 @@
 StartupEvents.registry("item", (event) => {
+  event
     .create("ptdye:incomplete_rail", "create:sequenced_assembly")
     .unstackable();
   event
@@ -8,3 +9,15 @@ StartupEvents.registry("item", (event) => {
     })
     .unstackable();
   event
+    .create("ptdye:incomplete_pizza", "create:sequenced_assembly")
+    .food((food) => {
+      food.hunger(2).saturation(1);
+    })
+    .unstackable();
+  event
+  .create("ptdye:pizza_base", "create:sequenced_assembly")
+  .food((food) => {
+    food.hunger(1).saturation(1);
+  })
+  .unstackable();
+});
