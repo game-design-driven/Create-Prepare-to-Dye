ef2b2663f0e7a36d1a5db21f1d1a799700b87e3f
Yarden Zamir <dev@yarden-zamir.com>
2024-02-07 22:05:55 +0200

    tweak: better dough and pancake recipes

--- kubejs/server_scripts/base/featrues/cooking.js
+++ kubejs/server_scripts/base/featrues/cooking.js
@@ -5,3 +5,15 @@ if (feature("Tomato sauce recipes")) {
     temperature.heated
   );
 }
+if (feature("Better dough recipes")) {
+  removeRecipe({ id: "create:crafting/appliances/dough" });
+  removeRecipe({ id: "create:mixing/dough_by_mixing" });
+  addMixing("create:dough", ["create:wheat_flour", "50mb water"]);
+}
+
+if (feature("Better pancake recipe")) {
+  addFluidToTag("ptdye:pancake_batter", [
+    "create:chocolate",
+    "create:honey",
+    "milk",
+  ]);
