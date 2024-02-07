a64cfaf1f886a3aaf45b6e7d9dbbbf333c2f20a2
Yarden Zamir <dev@yarden-zamir.com>
2024-02-07 22:06:10 +0200

    tweak: pizza recipe

--- kubejs/server_scripts/base/featrues/cooking.js
+++ kubejs/server_scripts/base/featrues/cooking.js
@@ -17,3 +17,25 @@ if (feature("Better pancake recipe")) {
     "create:honey",
     "milk",
   ]);
+
+  removeRecipe({ id: "supplementaries:pancake" });
+
+  ["200x create:chocolate", "200x create:honey", "200x milk"].forEach(
+    (pancake_batter) => {
+      addAssembly("4x supplementaries:pancake", "create:dough", [
+        addFilling("stick", "stick", pancake_batter),
+        addDeploying("stick", "stick", "egg"),
+        addDeploying("stick", "stick", "sugar"),
+        addPressing("stick", "stick"),
+      ]);
+    }
+  );
+}
+
+if (feature("Pizza recipes")) {
+  addPressing("ptdye:pizza_base", "create:dough");
+  addAssembly("ptdye:pizza", "ptdye:pizza_base", [
+    addFilling("ptdye:pizza", "ptdye:pizza_base", "ptdye:tomato_sauce"),
+    addDeploying("ptdye:pizza", "ptdye:pizza_base", "kubejs:fermented_blob"),
+  ]);
+}
