eba15396ba6bbae3797301aeb8bbab79b4007db4
Yarden Zamir <dev@yarden-zamir.com>
2024-02-07 22:03:12 +0200

    tweak: add incomplete pancake

--- kubejs/startup_scripts/custom_items/in_progress_items.js
+++ kubejs/startup_scripts/custom_items/in_progress_items.js
@@ -1,5 +1,10 @@
 StartupEvents.registry("item", (event) => {
-    event
-      .create("ptdye:incomplete_rail", "create:sequenced_assembly")
-      .unstackable()
-  });
+    .create("ptdye:incomplete_rail", "create:sequenced_assembly")
+    .unstackable();
+  event
+    .create("ptdye:incomplete_pancake", "create:sequenced_assembly")
+    .food((food) => {
+      food.hunger(2).saturation(1);
+    })
+    .unstackable();
+  event
