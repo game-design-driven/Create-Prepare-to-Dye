ba36ad1b3e04ad64e9587ae7c1d0bc5fb5a1e152
Auseawesome <ajthomasbtb@outlook.com>
2024-02-22 14:34:17 +0000

    Removed automatic namespacing function

--- kubejs/server_scripts/base/mods/craftingOnAStick.js
+++ kubejs/server_scripts/base/mods/craftingOnAStick.js
@@ -1,17 +1,14 @@
 if (feature('Remove crafting tables on a stick')) {
     removeRecipe({mod:'crafting_on_a_stick'})
-    let namespace = (string) => {
-      return 'crafting_on_a_stick:' + string
-    }
     removeItems([
-      namespace('crafting_table'),
-      namespace('loom'),
-      namespace('grindstone'),
-      namespace('cartography_table'),
-      namespace('smithing_table'),
-      namespace('anvil'),
-      namespace('chipped_anvil'),
-      namespace('damaged_anvil')
+      'crafting_on_a_stick:crafting_table',
+      'crafting_on_a_stick:loom',
+      'crafting_on_a_stick:grindstone',
+      'crafting_on_a_stick:cartography_table',
+      'crafting_on_a_stick:smithing_table',
+      'crafting_on_a_stick:anvil',
+      'crafting_on_a_stick:chipped_anvil',
+      'crafting_on_a_stick:damaged_anvil'
     ])
     addShaped(namespace('stonecutter'), ['s','r'], { s: 'minecraft:stonecutter', r: '#forge:rods' })
 }
