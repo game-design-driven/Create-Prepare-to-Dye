c7dd5c5938d123600bca3392a07553bced18e956
Auseawesome <ajthomasbtb@outlook.com>
2024-02-22 13:31:01 +0000

    Removed all crafting on a stick items
    Also edited recipe for the stonecutter so it can be made without any covers if you somehow lose it...

--- kubejs/server_scripts/base/mods/minecraft.js
+++ kubejs/server_scripts/base/mods/minecraft.js
@@ -546,3 +546,21 @@ if (feature('Replace bucket recipe with assembler recipe')) {
   removeRecipe({id:'minecraft:bucket'})
   addStonecutting('bucket', '#forge:ingots/iron')
 }
+
+if (feature('Remove crafting tables on a stick')) {
+  removeRecipe({mod:'crafting_on_a_stick'})
+  let namespace = (string) => {
+    return 'crafting_on_a_stick:' + string
+  }
+  removeItems([
+    namespace('crafting_table'),
+    namespace('loom'),
+    namespace('grindstone'),
+    namespace('cartography_table'),
+    namespace('smithing_table'),
+    namespace('anvil'),
+    namespace('chipped_anvil'),
+    namespace('damaged_anvil')
+  ])
+  addShaped(namespace('stonecutter'), ['s','r'], { s: 'minecraft:stonecutter', r: '#forge:rods' })
+}
