6127968a3583e1d1788564cbfea9d19ac4e7bcfc
Yarden Zamir <dev@yarden-zamir.com>
2024-10-21 23:22:43 +0300

    feat: add trading debug command to manage trading platform index and spawn some items

--- kubejs/server_scripts/base/debugtools.js
+++ kubejs/server_scripts/base/debugtools.js
@@ -14,4 +14,27 @@ ServerEvents.commandRegistry((event) => {
     })
   );
 });
+  event.register(
+    Commands.literal("trading").executes((context) => {return 0})
+    .then(Commands.literal("getPostageStamp").executes((context) => {
+      let postageStamp = Item.of("ptdye:postage_stamp_transceiver");
+      context.getSource().player.give(postageStamp);
+      return 0;
+    }))
+    .then(Commands.literal("getTradingTransceiver").executes((context) => {
+      let tradingTransceiver = Item.of("ptdye:trading_transceiver");
+      context.getSource().player.give(tradingTransceiver);
+      return 0;
+    }))
+    .then(Commands.literal("platform").executes((context) => {return 0})
+    .then(Commands.literal("list").executes((context) => {
+      //prints all trading platforms with ids and cordinates eg : "1: 78,11867"
+      let existing_platforms_x = Utils.server.persistentData.getIntArray("existing_platforms_x");
+      let existing_platforms_z = Utils.server.persistentData.getIntArray("existing_platforms_z");
+      for (let index = 0; index < existing_platforms_x.length; index++) {
+        let x = existing_platforms_x[index];
+        let z = existing_platforms_z[index];
+        context.getSource().player.tell(Text.of(index + ": " + x + "," + z));
+      }
+      return 0;
 
