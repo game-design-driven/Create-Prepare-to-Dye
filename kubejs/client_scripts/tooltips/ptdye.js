4b362512f9b08afba639826aec49ea65f1222acc
Yarden Zamir <dev@yarden-zamir.com>
2024-10-22 08:23:28 +0300

    docs: update tooltip of trading tranciever to mention postage_stamp_transceiver

--- kubejs/client_scripts/tooltips/ptdye.js
+++ kubejs/client_scripts/tooltips/ptdye.js
@@ -1,6 +1,7 @@
 addTooltip(
   "ptdye:trading_transceiver",
-  "A single-use transceiver used to request an _Oscar-Delta™_ class Trading Platform® for all your inter-union trading needs. _Right click_ to request a trading platform near your current location. Expect a delivery time of up to 2-3 business days. Terms and conditions may apply. Note: The trading platform _cannot be moved_ after it lands."
+  "A single-use transceiver used to request an _Oscar-Delta™_ class Trading Platform® for all your inter-union trading needs. _Right click_ to request a trading platform near your current location. Expect a delivery time of up to 2-3 business days. Terms and conditions may apply. Note: The trading platform _cannot be moved_ after it lands. Unless using the _Intergalactic Postage Stamp Transceiver_ to return the platform to the company."
+);
 addTooltip(
   "ptdye:postage_stamp_transceiver",
   "A single-use transceiver used to return your _Oscar-Delta™_ class Trading Platform® to the _United SPACE consumer union plus plus®_ company. _Right click_ to return your trading platform to the company. Gives you back an _Trading Platform® request transceiver_"
