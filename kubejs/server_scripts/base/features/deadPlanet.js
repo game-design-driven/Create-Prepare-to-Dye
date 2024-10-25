600c6b072c7489e18d964df140c32d4ab9906881
Yarden Zamir <dev@yarden-zamir.com>
2024-10-25 20:42:43 +0300

    fix: remove unintended tree_fertilizer recipe

--- kubejs/server_scripts/base/features/deadPlanet.js
+++ kubejs/server_scripts/base/features/deadPlanet.js
@@ -18,7 +18,7 @@ addMilling(['stick','stick %75'], 'dead_bush')
 addFilling('sand', 'red_sand', '100x milk')
 
 addAltar('2x bone_meal', ['bone_meal', '4x kubejs:fermented_blob'], 1000)
-addAltar('create:tree_fertilizer', ['2x bone_meal','#forge:coral', '8x kubejs:fermented_blob'], 7000)
+// addAltar('create:tree_fertilizer', ['2x bone_meal','#forge:coral', '8x kubejs:fermented_blob'], 7000)
 
 addDripping('grass', 'dead_bush')
 
