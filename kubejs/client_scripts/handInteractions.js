2660aaef98376357b49d20da8378aeaa69f037e8
Yarden Zamir <dev@yarden-zamir.com>
2024-11-11 19:28:38 +0200

    fix: potential bug where client cancels right click event animation and sound

--- kubejs/client_scripts/handInteractions.js
+++ kubejs/client_scripts/handInteractions.js
@@ -1,6 +1,8 @@
 BlockEvents.rightClicked("createdieselgenerators:diesel_engine", (event) => {
+  if (event.block != "createdieselgenerators:diesel_engine") return;
   event.cancel();
 });
 BlockEvents.rightClicked("create:spout", (event) => {
+  if (event.block != "create:spout") return;
   event.cancel();
 });
