a3842f8a84ea7148f7ebe5bc59e6e0d6f47cb3ee
Yarden Zamir <dev@yarden-zamir.com>
2025-12-22 21:20:59 +0200

    Update player_tweaks.js

--- kubejs/server_scripts/player_tweaks.js
+++ kubejs/server_scripts/player_tweaks.js
@@ -2,8 +2,82 @@
 // Player Tweaks - player-related gameplay modifications
 
 // Player invincibility - players cannot take damage except from void/kill
-EntityEvents.hurt(function(event) {
+EntityEvents.hurt(function (event) {
   if (event.entity.player && !event.source.isBypassInvul()) {
+    event.cancel();
+  }
+});
+
+// =============================================================================
+// HAND INTERACTIONS - Fill blocks directly with buckets
+// =============================================================================
+
+function fillBlockWithBucket(block, event) {
+  var fluid;
+  var amount = 1000;
+  var heldItem = event.player.handSlots[0].id;
+
+  if (heldItem === "minecraft:milk_bucket") {
+    fluid = "minecraft:milk";
+  } else {
+    fluid = heldItem.replace("_bucket", "");
+  }
+
+  if (!event.player.isCreative()) {
+    event.player.setMainHandItem("minecraft:bucket");
+  }
+
+  event.level.runCommandSilent(
+    "playsound minecraft:item.bucket.empty block @a " +
+      event.block.x + " " + event.block.y + " " + event.block.z + " 0.4 1.7"
+  );
+
+  block.mergeEntityData({
+    Tanks: [
+      {
+        TankContent: {
+          Amount: amount,
+          FluidName: fluid,
+        },
+      },
+    ],
+  });
+}
+
+// Fill diesel engine with bucket
+BlockEvents.rightClicked("createdieselgenerators:diesel_engine", function (event) {
+  var heldItem = event.player.handSlots[0].id;
+  if (
+    heldItem.endsWith("_bucket") &&
+    event.block.getEntityData().Tanks[0].Level.Value === 0
+  ) {
+    fillBlockWithBucket(event.block, event);
+    event.cancel();
+  }
+});
+
+// Fill spout with bucket
+BlockEvents.rightClicked("create:spout", function (event) {
+  var heldItem = event.player.handSlots[0].id;
+  if (
+    heldItem.endsWith("_bucket") &&
+    event.block.getEntityData().Tanks[0].Level.Value === 0
+  ) {
+    var amount = 1000;
+    var fluid = heldItem.replace("_bucket", "");
+    event.player.swing();
+
+    Utils.server.runCommandSilent(
+      "setblock " + event.block.x + " " + event.block.y + " " + event.block.z + " air"
+    );
+    Utils.server.runCommandSilent(
+      "setblock " + event.block.x + " " + event.block.y + " " + event.block.z +
+      " create:spout{Tanks:[{TankContent:{Amount:" + amount + ",FluidName:\"" + fluid + "\"}}]}"
+    );
+
+    if (!event.player.isCreative()) {
+      event.player.setMainHandItem("minecraft:bucket");
+    }
     event.cancel();
   }
 });
